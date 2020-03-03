b8e13ea08124e0082823279bd11673f40cdbd438
Joshnee Kim B. Cunanan <joshnee.cunanan@ptcc.design>
2020-03-03 10:54:23 +0800

    added logger

--- python_source/purge_audit_automation/src/purge_audit_script.py
+++ python_source/purge_audit_automation/src/purge_audit_script.py
@@ -3,6 +3,7 @@
 from comtypes import client
 from comtypes import COMError
 from shutil import copyfile
+from logger import Logger
 import os
 
 BRICSCAD_APP_NAME = "BricscadApp.AcadApplication"
@@ -26,6 +27,8 @@ class PurgeAuditScript:
         self.input_directory = os.path.join(self.root_directory, "input")
         self.output_directory = os.path.join(self.root_directory, "output")
         self.error_directory = os.path.join(self.output_directory, "error")
+        logger = Logger()
+        self.logger = logger.get_logger()
 
     def begin_automation(self):
         self.__traverse_in_directory()
@@ -47,43 +50,42 @@ class PurgeAuditScript:
             self.copy_file_with_extension(drawing_full_path)
         else:
             self.copy_file_with_extension(drawing_full_path, has_error=True)
+        self.clean_up_files()
 
     def __open_file(self, drawing_full_path):
-        print("Opening File: {}".format(drawing_full_path))
+        self.logger.info("Opening File: {}".format(drawing_full_path))
         document = None
         if os.path.exists(drawing_full_path):
             try:
                 self.bricscad_application.Documents.Open(drawing_full_path)
                 document = self.bricscad_application.ActiveDocument
             except COMError:
-                print("[ERROR]Invalid Drawing File!: {}".format(drawing_full_path))
+                self.logger.error("[ERROR]Invalid Drawing File!: {}".format(drawing_full_path))
         return document
 
     def __purge_document(self, document, file_name):
         if document:
             for i in range(0, 5):
-                print("[ATTEMPT{}]Purging Drawing: {}...".format(i+1, file_name))
+                self.logger.info("[ATTEMPT{}]Purging Drawing: {}...".format(i+1, file_name))
                 document.PurgeAll()
-        print("Purging Drawing Done...")
+        self.logger.info("Purging Drawing Done...")
 
     def __audit_document(self, document, file_name):
-        print("Auditing Drawing: {}...".format(file_name))
+        self.logger.info("Auditing Drawing: {}...".format(file_name))
         if document:
             document.AuditInfo(True)
-        print("Auditing Drawing Done...")
+        self.logger.info("Auditing Drawing Done...")
 
     def __save_document(self, document, file_name):
-        print("Saving changes to Drawing: {}".format(file_name))
+        self.logger.info("Saving changes to Drawing: {}".format(file_name))
         if not document.Saved:
-            print("There are unsaved changes in the Drawing")
+            self.logger.info("There are unsaved changes in the Drawing")
             document.Save()
             self.bricscad_application.Documents.Close()
-        print("Saving done and Closed...")
+        self.logger.info("Saving done and Closed...")
 
     def copy_file_with_extension(self, drawing_full_path, has_error=False):
         file_relative_path = drawing_full_path.replace(self.input_directory + os.sep, "")
-        file_relative_path = os.path.normpath(file_relative_path)
-
         file_directory = os.path.dirname(file_relative_path)
         file_directory = os.path.normpath(file_directory)
         path_list = file_directory.split(os.sep)
@@ -97,15 +99,16 @@ class PurgeAuditScript:
             try:
                 copyfile(drawing_full_path, file_path_to_output)
             except FileExistsError:
-                print("File {} already exists!".format(os.path.basename(drawing_full_path)))
+                self.logger.warn("File {} already exists!".format(os.path.basename(drawing_full_path)))
         elif has_error:
             file_path_to_output = os.path.join(self.error_directory, file_name)
             try:
                 copyfile(drawing_full_path, file_path_to_output)
             except FileExistsError:
-                print("File {} already exists!".format(file_name))
+                self.logger.warn("File {} already exists!".format(file_name))
 
     def create_file_hierarchy(self, path_list):
+        self.logger.info("Creating file hierarchy with path list: {}".format(path_list))
         try:
             # create folders
             for directory in path_list:
@@ -115,7 +118,8 @@ class PurgeAuditScript:
                     os.makedirs(file_heirarchy_directory)
 
         except FileExistsError:
-            print("[WARNING] Directory already exists.")
+            self.logger.warn("Directory already exists.")
+        self.logger.info("Creating file hierarchy done.")
 
     def clean_up_files(self):
         for dir_path, dir_names, file_names in os.walk(self.input_directory):
