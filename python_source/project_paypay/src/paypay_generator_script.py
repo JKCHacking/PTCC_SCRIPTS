4f386df415f7b7444eeadcd0b2463c2e821de90a
Joshnee Kim B. Cunanan <joshnee.cunanan@ptcc.design>
2020-03-12 09:10:14 +0800

    added implementation for determining the divided for a given tile length

--- python_source/project_paypay/src/paypay_generator_script.py
+++ python_source/project_paypay/src/paypay_generator_script.py
@@ -19,17 +19,13 @@ class PayPayGenerator:
         hypotenuse = self.tile_length
         adjacent = round(sqrt((hypotenuse ** 2) - (opposite ** 2)), 1)
 
-        angle = degrees(acos((adjacent * adjacent + opposite * opposite - hypotenuse * hypotenuse)/(2.0 * adjacent * opposite)))
-        divided_angle = round(angle/8.00, 2)
+        angle = round(degrees(acos((adjacent**2 + hypotenuse**2 - opposite**2)/(2.0 * adjacent * hypotenuse))), 2)
+        divided_angle = round(angle / 8.00, 2)
 
-        print(adjacent)
-        print(f"Angle: {angle}")
-        print(f"Divided Angle: {divided_angle}")
-
-        print(acos(((1398.7) ** 2)))
+        return divided_angle
 
 
 if __name__ == "__main__":
     tile_len = 1423.0
     pp_gen = PayPayGenerator(tile_len)
-    pp_gen.calculate_divided_angle()
+    div_angle = pp_gen.calculate_divided_angle()
