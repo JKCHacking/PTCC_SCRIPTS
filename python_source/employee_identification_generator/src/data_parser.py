6d866ce33e5468f4b2bc01143920cbfcf4b25d5e
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-07-07 14:03:31 +0800

    fixed conversion of date string to date object

--- python_source/employee_identification_generator/src/data_parser.py
+++ python_source/employee_identification_generator/src/data_parser.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import datetime
+import dateutil
 from src.constants import Constants
 from src.logger import Logger
 
@@ -14,8 +16,21 @@ class DataParser:
         with open(csv_path, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                self.employee_list.append(row)
+                hired_date_obj = self.convert_date(row['hired_date'])
+                employee_dict = {'name': row['name'], 'hired_date': hired_date_obj}
+                self.employee_list.append(employee_dict)
         return self.employee_list
+
+    @staticmethod
+    def convert_date(date_string):
+        date_obj = None
+        try:
+            date_obj = dateutil.parser.parse(date_string)
+            date_obj = date_obj.strftime(Constants.DATE_FORMAT)
+            date_obj = datetime.datetime.strptime(date_obj, Constants.DATE_FORMAT)
+        except (TypeError, ValueError):
+            print(f"You have input an Invalid date {date_string}")
+        return date_obj
 
     def create_csv_output(self, sorted_employee_list):
         output_path = os.path.join(Constants.OUTPUT_DIR, "employee_list.csv")
