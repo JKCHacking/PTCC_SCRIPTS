8a697505bd691299cf2df70e8122a5de16f5ab58
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-07-07 14:19:28 +0800

    fixed date to have leading zeroes if 1 digit only

--- python_source/employee_identification_generator/src/id_creator.py
+++ python_source/employee_identification_generator/src/id_creator.py
@@ -13,7 +13,10 @@ class IdCreator:
             if date_tracker != employee["hired_date"]:
                 index = 0
                 date_tracker = employee["hired_date"]
-            emp_id = f"{date_tracker.date}{date_tracker.month}{date_tracker.year}{index}"
+            day = str(date_tracker.day).zfill(2)
+            month = str(date_tracker.month).zfill(2)
+            year = str(date_tracker.year).zfill(2)
+            emp_id = f"{day}{month}{year}{index}"
             employee.update({"name": employee["name"], "hired_date": employee["hired_date"], "id": emp_id})
             index += 1
         return sorted_employee_list
