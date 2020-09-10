5ac81e3039921d5aa59f9d0d1ee6551eb078469b
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-09-10 11:09:37 +0800

    [DrawingSectionProperty] added tests and testdata

--- python_source/DrawingSectionProperty/test/fea/pre_processor_test.py
+++ python_source/DrawingSectionProperty/test/fea/pre_processor_test.py
@@ -41,3 +41,27 @@ class PreProcessorTest(unittest.TestCase):
         self.assertEqual(1, len(geometry_list))
         self.assertEqual(2, len(geometry.holes))
         geometry.plot_geometry()
+
+    def test_create_geometry_case_3(self):
+        segment_size = 0.25
+        has_holes = False
+        pre_processor = PreProcessor(segment_size, has_holes)
+
+        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", "testdata006.dxf")
+        geometry_list = pre_processor.create_geometry(testdata_file_path)
+        geometry = geometry_list[0]
+        self.assertEqual(1, len(geometry_list))
+        self.assertEqual(0, len(geometry.holes))
+        geometry.plot_geometry()
+
+    def test_create_geometry_case_3_complex(self):
+        segment_size = 0.25
+        has_holes = False
+        pre_processor = PreProcessor(segment_size, has_holes)
+
+        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", "testdata007.dxf")
+        geometry_list = pre_processor.create_geometry(testdata_file_path)
+        geometry = geometry_list[0]
+        self.assertEqual(1, len(geometry_list))
+        self.assertEqual(0, len(geometry.holes))
+        geometry.plot_geometry()
