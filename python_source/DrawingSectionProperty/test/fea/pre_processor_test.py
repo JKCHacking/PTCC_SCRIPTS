8ff7f438a0984fc6be589e55214115f5f5cc83e1
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-09-10 11:35:08 +0800

    [DrawingSectionProperty] Added tests and testdata

--- python_source/DrawingSectionProperty/test/fea/pre_processor_test.py
+++ python_source/DrawingSectionProperty/test/fea/pre_processor_test.py
@@ -65,3 +65,54 @@ class PreProcessorTest(unittest.TestCase):
         self.assertEqual(1, len(geometry_list))
         self.assertEqual(0, len(geometry.holes))
         geometry.plot_geometry()
+
+    def test_create_geometry_case_4_complex(self):  # LIMITATION
+        segment_size = 0.25
+        has_holes = False
+        pre_processor = PreProcessor(segment_size, has_holes)
+
+        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", "testdata008.dxf")
+        geometry_list = pre_processor.create_geometry(testdata_file_path)
+        geometry1 = geometry_list[0]
+        geometry2 = geometry_list[1]
+        self.assertEqual(2, len(geometry_list))
+        self.assertEqual(0, len(geometry1.holes))
+        self.assertEqual(0, len(geometry2.holes))
+        geometry1.plot_geometry()
+        geometry2.plot_geometry()
+
+    def test_create_geometry_case_5_complex(self):
+        segment_size = 0.25
+        has_holes = True
+        pre_processor = PreProcessor(segment_size, has_holes)
+
+        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", "testdata008.dxf")
+        geometry_list = pre_processor.create_geometry(testdata_file_path)
+        self.assertEqual(0, len(geometry_list))
+
+    def test_create_geometry_case_6(self):
+        segment_size = 0.25
+        has_holes = True
+        pre_processor = PreProcessor(segment_size, has_holes)
+
+        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", "testdata009.dxf")
+        geometry_list = pre_processor.create_geometry(testdata_file_path)
+        geometry1 = geometry_list[0]
+        geometry2 = geometry_list[1]
+        self.assertEqual(2, len(geometry_list))
+        self.assertEqual(2, len(geometry1.holes))
+        self.assertEqual(1, len(geometry2.holes))
+        geometry1.plot_geometry()
+        geometry2.plot_geometry()
+
+    def test_create_geometry_case_7(self):
+        segment_size = 0.25
+        has_holes = True
+        pre_processor = PreProcessor(segment_size, has_holes)
+
+        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", "testdata010.dxf")
+        geometry_list = pre_processor.create_geometry(testdata_file_path)
+        geometry = geometry_list[0]
+        self.assertEqual(1, len(geometry_list))
+        self.assertEqual(2, len(geometry.holes))
+        geometry.plot_geometry()
