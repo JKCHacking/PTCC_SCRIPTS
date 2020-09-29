2987ae738f220a25079247169d2854ea08e3d888
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-09-29 18:01:34 +0800

    [DrawingSectionProperty] Fixed test errors inconsistent fileinput name

--- python_source/DrawingSectionProperty/test/script/section_property_reporter_test.py
+++ python_source/DrawingSectionProperty/test/script/section_property_reporter_test.py
@@ -8,10 +8,11 @@ from src.util.constants import Constants
 class SectionPropertyReporterTest(unittest.TestCase):
 
     def test_script_01(self):
-        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", "testdata001.dxf")
+        file_input = "testdata001.dxf"
+        testdata_file_path = os.path.join(Constants.TEST_DIR, "testdata", file_input)
         
         src = testdata_file_path
-        dest = os.path.join(Constants.INPUT_DIR, "testdata004.dxf")
+        dest = os.path.join(Constants.INPUT_DIR, file_input)
         copyfile(src, dest)
         main_fp = os.path.join(Constants.ROOT_DIR, 'main.py')
         command_str = f"python {main_fp} -long -hole"
@@ -22,4 +23,4 @@ class SectionPropertyReporterTest(unittest.TestCase):
         output, error = process.communicate()
         print(output.decode("utf-8"))
         os.remove(dest)
-        self.assertTrue(os.path.exists(os.path.join(Constants.OUTPUT_DIR, "testdata001.pdf")))
+        self.assertTrue(os.path.exists(os.path.join(Constants.OUTPUT_DIR, file_input)))
