ab094fd09183bcf09db9de9281d89c3c2170769f
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-10-05 13:07:58 +0800

    [SCAM_PROJECT] Added implementation for getting the IP and the host name of the client upon visiting the fakesite.
    also added implementation of closing the tab.

--- python_source/SCAM_PROJECT/fakesite/models.py
+++ python_source/SCAM_PROJECT/fakesite/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    pc_name = models.CharField(max_length=10)
+    ip_add = models.CharField(max_length=10)
--- python_source/SCAM_PROJECT/fakesite/views.py
+++ python_source/SCAM_PROJECT/fakesite/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import socket
+from django.http import HttpResponse
+from ipware import get_client_ip
+from .models import User
+from pywinauto.keyboard import send_keys
 
-# Create your views here.
+
+def index(request):
+    ip, is_routable = get_client_ip(request)
+    host_info = socket.gethostbyaddr(ip)
+    pc_name = host_info[0]  # name of the computer
+    print(ip)
+    print(pc_name)
+    # save to database
+    user = User.objects.get_or_create(
+        pc_name=pc_name,
+        ip_add=ip
+    )
+    print(user)
+    send_keys("^w")
+    return HttpResponse("Thank you for visiting.")
