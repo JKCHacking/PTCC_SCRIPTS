31ed5a3d80b709de4542407d0366c7c7e61eb42c
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-10-05 16:00:35 +0800

    [SCAM_PROJECT] added static directory

--- python_source/SCAM_PROJECT/SCAM_PROJECT/settings.py
+++ python_source/SCAM_PROJECT/SCAM_PROJECT/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'pj666@6g3s!3wwp^_vtz%@^cms(3^lwg&40vz=@^6=2a=7(xqc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ptcccdesign.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- python_source/SCAM_PROJECT/fakesite/views.py
+++ python_source/SCAM_PROJECT/fakesite/views.py
@@ -2,23 +2,27 @@ import socket
 from django.http import HttpResponse
 from ipware import get_client_ip
 from .models import User
-from pynput.keyboard import Key, Controller
+# from pynput.keyboard import Key, Controller
 
 
 def index(request):
-    keyboard = Controller()
+    # keyboard = Controller()
     ip, is_routable = get_client_ip(request)
     host_info = socket.gethostbyaddr(ip)
     pc_name = host_info[0]  # name of the computer
+    pc_name2 = request.META['REMOTE_HOST']
+    pc_name3 = request.META['HTTP_HOST']
     print(ip)
     print(pc_name)
+    print(pc_name2)
+    print(pc_name3)
     # save to database
     user = User.objects.get_or_create(
         pc_name=pc_name,
         ip_add=ip
     )
     print(user)
-    with keyboard.pressed(Key.ctrl):
-        keyboard.press('w')
-        keyboard.release('w')
+    # with keyboard.pressed(Key.ctrl):
+    #     keyboard.press('w')
+    #     keyboard.release('w')
     return HttpResponse("Thank you for visiting.")
