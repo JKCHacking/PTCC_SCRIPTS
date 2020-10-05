ea68cbc855f58df90ae97f0edc4c8a1cd8081e2c
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-10-05 14:16:21 +0800

    [SCAM_PROJECT] removed pywinauto implementation and used pynput instead

--- python_source/SCAM_PROJECT/fakesite/views.py
+++ python_source/SCAM_PROJECT/fakesite/views.py
@@ -2,10 +2,11 @@ import socket
 from django.http import HttpResponse
 from ipware import get_client_ip
 from .models import User
-from pywinauto.keyboard import send_keys
+from pynput.keyboard import Key, Controller
 
 
 def index(request):
+    keyboard = Controller()
     ip, is_routable = get_client_ip(request)
     host_info = socket.gethostbyaddr(ip)
     pc_name = host_info[0]  # name of the computer
@@ -17,5 +18,7 @@ def index(request):
         ip_add=ip
     )
     print(user)
-    send_keys("^w")
+    with keyboard.pressed(Key.ctrl):
+        keyboard.press('w')
+        keyboard.release('w')
     return HttpResponse("Thank you for visiting.")
