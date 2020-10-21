285882282b16d6691470a9818196fdf94c704892
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-10-21 16:02:54 +0800

    [DrawingScanner2] Fixed code structure to be modular
    modified test accordingly
    added new testdata

--- python_source/DrawingScanner2/src/script.py
+++ python_source/DrawingScanner2/src/script.py
@@ -1,9 +1,11 @@
 import os
-import csv
-from openpyxl import Workbook
+import math
 import ezdxf
 import ezdxf.math
-from ezdxf.tools.rgb import DXF_DEFAULT_COLORS, int2rgb
+from openpyxl import Workbook
+from openpyxl.styles.colors import Color
+from openpyxl.styles.fills import PatternFill
+from ezdxf.tools.rgb import DXF_DEFAULT_COLORS, DXF_DEFAULT_COLORS_2, int2rgb
 from src.util.constants import Constants
 
 
@@ -11,70 +13,139 @@ class Script:
     """
         Gets all polylines from a dxf file and records the handle, width, and height of each polyline.
     """
+    def __init__(self):
+        self.unit = None
+
     def iter_input(self):
+        """
+            gets every file in the output folder and process the necessary data
+        """
         input_dir = Constants.INPUT_DIR
         for dir_path, dir_names, file_names in os.walk(input_dir):
             for file_name in file_names:
                 file_full_path = os.path.join(dir_path, file_name)
                 if file_full_path.endswith(Constants.DXF_FILE_EXT):
-                    self.create_output(file_full_path)
+                    # for every dxf file you create 1 excel file
+                    workbook = Workbook()
+                    # getting all the blocks
+                    for block_ent in self.__get_blocks(file_full_path):
+                        # polyline container
+                        pl_list = []
+                        # getting all the polylines in the block reference
+                        for entity in block_ent.virtual_entities():
+                            type = entity.dxftype()
+                            if type == "LWPOLYLINE" or type == "POLYLINE":
+                                found = False
+                                count = 1
+                                width, height = self.__compute_width_height(entity)
+                                rgb_str = self.__get_rgb(entity)
+                                # checking if same polyline already exist in the list.
+                                for ent_member in pl_list:
+                                    if ent_member['A'] == rgb_str and ent_member['C'] == height:
+                                        ent_member['D'] += 1
+                                        found = True
+                                        break
+                                if not found:
+                                    pl_data_dict = self.__compose_data_dict(rgb_str, width, height, count)
+                                    pl_list.append(pl_data_dict)
+                        block_name = block_ent.dxf.name
+                        # for every block you create 1 worksheet
+                        self.__create_spreadsheet(workbook, block_name, pl_list)
+                    filename_ext = os.path.basename(file_full_path)
+                    filename = os.path.splitext(filename_ext)[0]
+                    output_dir = os.path.join(Constants.OUTPUT_DIR, filename + '.xlsx')
+                    if len(workbook.worksheets) > 1:
+                        default_ws = workbook["Sheet"]
+                        workbook.remove(default_ws)
+                        workbook.save(output_dir)
+                    else:
+                        print(f"No BlockReference Found inside file: {file_full_path}")
 
-    def create_output(self, filename_fp):
-        dxf_file = ezdxf.readfile(filename_fp)
-        filename_ext = os.path.basename(filename_fp)
-        filename = os.path.splitext(filename_ext)[0]
-        output_dir = os.path.join(Constants.OUTPUT_DIR, filename + '.xlsx')
-        ms = dxf_file.modelspace()
+    def __create_spreadsheet(self, workbook, block_name, ent_list):
+        # add unit conversions that are necessary
+        new_unit = self.unit
+        if self.unit == "Inches":
+            new_unit = "Feet-Inch"
+        fieldnames = ['RGBColor', f'Width ({new_unit})', f'Height ({new_unit})', 'Count']
+        worksheet = workbook.create_sheet(f"BlockReference {block_name}")
+        worksheet.append(fieldnames)
+        for i, ent_prop in enumerate(ent_list):
+            worksheet.append(ent_prop)
+            color = Color(rgb=ent_prop['A'])
+            fill = PatternFill(patternType='solid', fgColor=color)
+            worksheet[f'A{i+2}'].fill = fill
+
+    def __compose_data_dict(self, rgb_color, width, height, count):
+        ent_prop_dict = {
+            'A': rgb_color,
+            'B': width,
+            'C': height,
+            'D': count
+        }
+        return ent_prop_dict
+
+    def __get_rgb(self, polyline):
+        # getting the autocad color index of the polyline
+        ent_aci = polyline.dxf.color
+        try:
+            ent_color_hex = DXF_DEFAULT_COLORS[ent_aci]
+        except IndexError:
+            try:
+                ent_color_hex = DXF_DEFAULT_COLORS_2[ent_aci]
+            except IndexError:
+                ent_color_hex = None
+        # if ent_color_hex:
+        #     ent_color_rgb = int2rgb(ent_color_hex)
+        #     ent_color_rgb_str = f"({ent_color_rgb[0]}, {ent_color_rgb[1]}, {ent_color_rgb[2]})"
+        # else:
+        #     ent_color_rgb_str = "Color Undefined"
+        try:
+            ent_color_hex_str = f"{ent_color_hex:06X}"
+        except TypeError:
+            ent_color_hex_str = "FFFFFF"
+        return ent_color_hex_str
+
+    def __compute_width_height(self, polyline):
+        # getting the points for creation of bounding box
+        ent_points = polyline.get_points('xy')
+        bbox = ezdxf.math.BoundingBox2d(ent_points)
+        extmin = bbox.extmin
+        extmax = bbox.extmax
+
+        # getting the extremas of the bounding box
+        xmax, ymax = extmax
+        xmin, ymin = extmin
+
+        # getting the width and the height of the polyline
+        w = round(xmax - xmin, 2)
+        h = round(ymax - ymin, 2)
+
+        # add unit conversions that are necessary
+        # convert inches to feet-inch
+        if self.unit == "Inches":
+            w = self.__convert_inches_to_feet_inch(w)
+            h = self.__convert_inches_to_feet_inch(h)
+        return w, h
+
+    def __get_blocks(self, dxf_fp):
+        dxf_file = ezdxf.readfile(dxf_fp)
         ins_unit_num = dxf_file.header["$INSUNITS"]
-        unit = Constants.INS_UNITS[ins_unit_num]
-        fieldnames = ['RGBColor', f'Width ({unit})', f'Height ({unit})', 'Count']
-
-        workbook = Workbook()
+        self.unit = Constants.INS_UNITS[ins_unit_num]
+        ms = dxf_file.modelspace()
         for ent in ms:
-            print("type:", ent.dxftype(), "handle:", ent.dxf.handle)
             if ent.dxftype() == "INSERT":
-                ent_list = []
-                handle = ent.dxf.handle
-                worksheet = workbook.create_sheet(f"BlockReference #{handle}")
-                worksheet.append(fieldnames)
+                block_ent = ent
+                yield block_ent
 
-                for blk_ent in ent.virtual_entities():
-                    type = blk_ent.dxftype()
-                    if type == "LWPOLYLINE" or type == "POLYLINE":
-                        found = False
-                        count = 1
-                        ent_points = blk_ent.get_points('xy')
-                        bbox = ezdxf.math.BoundingBox2d(ent_points)
-                        extmin = bbox.extmin
-                        extmax = bbox.extmax
+    def __convert_inches_to_feet_inch(self, inches):
+        # convert inches to feet
+        feet = round(inches / 12, 2)
+        # get the whole part and decimal part
+        decimal_feet, whole_feet = math.modf(feet)
+        whole_feet = int(whole_feet)
+        decimal_feet = round(decimal_feet, 2)
+        # convert the decimal part to inches
+        new_inches = round(decimal_feet * 12, 2)
 
-                        xmax, ymax = extmax
-                        xmin, ymin = extmin
-
-                        w = round(xmax - xmin, 2)
-                        h = round(ymax - ymin, 2)
-
-                        ent_aci = blk_ent.dxf.color
-                        ent_color_hex = DXF_DEFAULT_COLORS[ent_aci]
-                        ent_color_rgb = int2rgb(ent_color_hex)
-                        ent_color_rgb_str = f"({ent_color_rgb[0]}, {ent_color_rgb[1]}, {ent_color_rgb[2]})"
-
-                        for ent_member in ent_list:
-                            if ent_member['A'] == ent_color_rgb_str and ent_member['C'] == h:
-                                ent_member['D'] += 1
-                                found = True
-
-                        if not found:
-                            ent_prop_dict = {
-                                'A': ent_color_rgb_str,
-                                'B': w,
-                                'C': h,
-                                'D': count
-                            }
-                            ent_list.append(ent_prop_dict)
-                        print(f"type: {type}, ACI: {ent_aci} hex color: #{ent_color_hex :06X} "
-                              f"rgb color: {ent_color_rgb}")
-                print(" ")
-                for ent_prop in ent_list:
-                    worksheet.append(ent_prop)
-        workbook.save(output_dir)
+        feet_inches_fraction = f"{whole_feet}' - {new_inches}\""
+        return feet_inches_fraction
--- python_source/DrawingScanner2/test/script_test.py
+++ python_source/DrawingScanner2/test/script_test.py
@@ -9,66 +9,95 @@ from src.script import Script
 class ScriptTest(unittest.TestCase):
 
     def test_iter_input_01(self):
-        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", 'testdata001.dxf')
+        testdata_fname = 'testdata001.dxf'
+        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fname)
+
         source = testdata_file
-        dest = os.path.join(Constants.INPUT_DIR, 'testdata001.dxf')
+        dest = os.path.join(Constants.INPUT_DIR, testdata_fname)
         shutil.copyfile(source, dest)
 
         script = Script()
         script.iter_input()
 
+        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fname)
         filename_ext = os.path.basename(testdata_file)
         filename = os.path.splitext(filename_ext)[0]
-        expected_output_path = os.path.join(Constants.OUTPUT_DIR, filename + '.csv')
+        expected_output_path = os.path.join(Constants.OUTPUT_DIR, filename + '.xlsx')
+        dest = os.path.join(Constants.INPUT_DIR, testdata_fname)
+
         self.assertTrue(os.path.exists(expected_output_path))
-
-        # there should be 3 rows
-        expected_value_rows = [['7A', '4.2', '5'],
-                               ['7C', '4.1', '1.8'],
-                               ['7E', '3.9', '1.8']]
-
-        with open(expected_output_path, newline='') as csvfile:
-            # comparing the width and height of each polyline
-            reader = csv.DictReader(csvfile)
-            row_count = 0
-            for row in reader:
-                expected_handle = expected_value_rows[row_count][0]
-                expected_width = float(expected_value_rows[row_count][1])
-                expected_height = float(expected_value_rows[row_count][2])
-                self.assertEqual(row['handle'], expected_handle)
-                self.assertAlmostEqual(float(row['width']), expected_width)
-                self.assertAlmostEqual(float(row['height']), expected_height)
-                row_count += 1
-            self.assertEqual(len(expected_value_rows), row_count)
-        os.remove(expected_output_path)
+        # delete the generated files in the output folder
+        # os.remove(expected_output_path)
+        # delete the copied files in the input folder
         os.remove(dest)
 
-    def test_create_output_01(self):
-        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", 'testdata001.dxf')
-        script = Script()
-        script.create_output(testdata_file)
+    def test_iter_input_02(self):
+        testdata_fname = 'testdata002.dxf'
+        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fname)
 
-        # filename_ext = os.path.basename(testdata_file)
-        # filename = os.path.splitext(filename_ext)[0]
-        # expected_output_path = os.path.join(Constants.OUTPUT_DIR, filename + '.csv')
-        # self.assertTrue(os.path.exists(expected_output_path))
-        #
-        # # there should be 3 rows
-        # expected_value_rows = [['7A', '4.2', '5'],
-        #                        ['7C', '4.1', '1.8'],
-        #                        ['7E', '3.9', '1.8']]
-        #
-        # with open(expected_output_path, newline='') as csvfile:
-        #     # comparing the width and height of each polyline
-        #     reader = csv.DictReader(csvfile)
-        #     row_count = 0
-        #     for row in reader:
-        #         expected_handle = expected_value_rows[row_count][0]
-        #         expected_width = float(expected_value_rows[row_count][1])
-        #         expected_height = float(expected_value_rows[row_count][2])
-        #         self.assertEqual(row['handle'], expected_handle)
-        #         self.assertAlmostEqual(float(row['width']), expected_width)
-        #         self.assertAlmostEqual(float(row['height']), expected_height)
-        #         row_count += 1
-        #     self.assertEqual(len(expected_value_rows), row_count)
+        source = testdata_file
+        dest = os.path.join(Constants.INPUT_DIR, testdata_fname)
+        shutil.copyfile(source, dest)
+
+        script = Script()
+        script.iter_input()
+
+        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fname)
+        filename_ext = os.path.basename(testdata_file)
+        filename = os.path.splitext(filename_ext)[0]
+        expected_output_path = os.path.join(Constants.OUTPUT_DIR, filename + '.xlsx')
+        dest = os.path.join(Constants.INPUT_DIR, testdata_fname)
+
+        self.assertTrue(os.path.exists(expected_output_path))
+        # delete the generated files in the output folder
         # os.remove(expected_output_path)
+        # delete the copied files in the input folder
+        os.remove(dest)
+
+    def test_iter_input_03(self):
+        testdata_fname = 'testdata003.dxf'
+        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fname)
+
+        source = testdata_file
+        dest = os.path.join(Constants.INPUT_DIR, testdata_fname)
+        shutil.copyfile(source, dest)
+
+        script = Script()
+        script.iter_input()
+
+        testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fname)
+        filename_ext = os.path.basename(testdata_file)
+        filename = os.path.splitext(filename_ext)[0]
+        expected_output_path = os.path.join(Constants.OUTPUT_DIR, filename + '.xlsx')
+        dest = os.path.join(Constants.INPUT_DIR, testdata_fname)
+
+        self.assertTrue(os.path.exists(expected_output_path))
+        # delete the generated files in the output folder
+        # os.remove(expected_output_path)
+        # delete the copied files in the input folder
+        os.remove(dest)
+
+    def test_iter_input_04(self):
+        testdata_list = ['testdata001.dxf', 'testdata002.dxf', 'testdata003.dxf',
+                         'testdata004.dxf', 'testdata005.dxf', 'testdata006.dxf']
+        for testdata_fn in testdata_list:
+            testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fn)
+            source = testdata_file
+            dest = os.path.join(Constants.INPUT_DIR, testdata_fn)
+            shutil.copyfile(source, dest)
+
+        script = Script()
+        script.iter_input()
+
+        for testdata_fn in testdata_list:
+            testdata_file = os.path.join(Constants.TEST_DIR, "testdata", testdata_fn)
+            filename_ext = os.path.basename(testdata_file)
+            filename = os.path.splitext(filename_ext)[0]
+            expected_output_path = os.path.join(Constants.OUTPUT_DIR, filename + '.xlsx')
+            dest = os.path.join(Constants.INPUT_DIR, testdata_fn)
+
+            self.assertTrue(os.path.exists(expected_output_path))
+            # delete the generated files in the output folder
+            # os.remove(expected_output_path)
+            # delete the copied files in the input folder
+            # os.remove(dest)
