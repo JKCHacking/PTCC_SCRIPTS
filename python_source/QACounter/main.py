18c13f2ee2bd65764acff6c989958d2917a8b43d
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-10-28 13:04:20 +0800

    [QACounter] Removed Block name counter

--- python_source/QACounter/main.py
+++ python_source/QACounter/main.py
@@ -1,6 +1,5 @@
 import argparse
 from src.script.block_dim_counter import BlockDimCounter
-from src.script.block_name_counter import BlockNameCounter
 from src.script.mtext_counter import MTextCounter
 
 
@@ -8,18 +7,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-s',
                         type=str,
-                        choices=["bnc", "mtc", "bdc"],
+                        choices=["mtc", "bdc"],
                         nargs='*',
                         help='Support counter tools are: '
-                             '1. bnc (BlockNameCounter) - Counts names of a BlockReference and '
-                             'tally\'s each unique names '
-                             '2. mtc (MTextCounter) - Counts all the Mtext and tally each unique string value'
-                             '3. bdc (BlockDimCounter) - Counts all the polylines inside the block and '
+                             '1. mtc (MTextCounter) - Counts all the Mtext and tally each unique string value'
+                             '2. bdc (BlockDimCounter) - Counts all the polylines inside the block and '
                              'tally each with width and height of the polyline')
     args = parser.parse_args()
-    if args.s == "bnc":
-        script = BlockNameCounter()
-    elif args.s == "mtc":
+    if args.s == "mtc":
         script = MTextCounter()
     elif args.s == "bdc":
         script = BlockDimCounter()
