3419ee4756605bc1be4a83c0a9ec05f00ff4d585
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2020-12-21 09:15:03 +0800

    [LibreOfficeMacro] fixed the code for static document directory

--- python_source/LibreOfficeMacro/src/pdf_file_executor.py
+++ python_source/LibreOfficeMacro/src/pdf_file_executor.py
@@ -1,52 +1,42 @@
+import os
 import subprocess
 import threading
 import msgbox as util
 import uno
+from urllib.parse import urlparse
 
 
 def PdfFileExecutor(*args):
     '''Opens a pdf file linked from the spreadsheet'''
     evt = args[0]
-    button_name = evt.Source.Model.Name
     button_label = evt.Source.Model.Label
 
-    # MsgBox("Button Name: {} Button Label: {}".format(button_name, button_label))
+    desktop = XSCRIPTCONTEXT.getDesktop()
+    model = desktop.getCurrentComponent()
+    uri = model.getURL()
+    p = urlparse(uri)
+    doc_dir = os.path.dirname(p.path[1:-1])
+    # doc_dir = "X:/Office/joshnee/ADMIN_PROJECTS/libreoffice_hyperlink/with_macro"
 
-    # local_context = uno.getComponentContext()
-    # resolver = local_context.ServiceManager.createInstanceWithContext(
-    #     "com.sun.star.bridge.UnoUrlResolver", local_context
-    # )
-    # ctx = resolver.resolve("uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext")
-    # smgr = ctx.ServiceManager
-    # desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx)
+    active_sheet = model.CurrentController.ActiveSheet
+    cell1 = active_sheet.getCellRangeByName("A1")
+    cell1.String = str(uri)
 
-    # desktop = XSCRIPTCONTEXT.getDesktop()
-    # model = desktop.getCurrentComponent()
-    #
-    # active_sheet = model.CurrentController.ActiveSheet
-    # cell1 = active_sheet.getCellRangeByName("A1")
-    # cell1.String = str(dir(args))
-    # cell2 = active_sheet.getCellRangeByName("A2")
-    # cell2.String = str(type(args))
-
-    t1 = threading.Thread(target=pdf_thread_func, args=(button_label,))
+    t1 = threading.Thread(target=pdf_thread_func, args=(button_label, doc_dir))
     t1.start()
 
 
-def pdf_thread_func(button_label):
-    MsgBox("Button label: {}".format(button_label))
-    if button_label == "test.pdf page 10":
+def pdf_thread_func(button_label, document_dir):
+    MsgBox(document_dir)
+    file_name, page_num = button_label.split(" ")
+    if file_name and page_num:
         subprocess.run(["C:/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/AcroRd32.exe",
                         "/A",
-                        "page=10",
-                        "H:/Desktop/projects/libreoffice_hyperlink/test.pdf"],
+                        page_num,
+                        os.path.join(document_dir, file_name)],
                        shell=True)
-    elif button_label == "test2.pdf page 5":
-        subprocess.run(["C:/Program Files (x86)/Adobe/Acrobat Reader DC/Reader/AcroRd32.exe",
-                        "/A",
-                        "page=5",
-                        "H:/Desktop/projects/libreoffice_hyperlink/test2.pdf"],
-                       shell=True)
+    else:
+        MsgBox("Invalid Button Name format!")
 
 
 def MsgBox(txt):
