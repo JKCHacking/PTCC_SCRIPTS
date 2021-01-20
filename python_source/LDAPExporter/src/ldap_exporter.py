c24e66ff4af6f1a5cccf489adff09c850ba84ebd
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-01-20 14:05:00 +0800

    [LDAPExporter] added and implementation for converting raw ldap data to a python dictionary and exporting of dictionary data to .docx file. added new test codes accordingly

--- python_source/LDAPExporter/src/ldap_exporter.py
+++ python_source/LDAPExporter/src/ldap_exporter.py
@@ -1,17 +1,17 @@
 import os
+from docx import Document
+from docx.shared import Mm
+from docx.oxml.ns import qn
 from src.util.constants import Constants
 
 
 class LdapExporter:
-    def iter_input(self):
-        input_dir = Constants.INPUT_DIR
-        for dir_path, dir_names, file_names in os.walk(input_dir):
-            for file_name in file_names:
-                file_full_path = os.path.join(dir_path, file_name)
-                if file_full_path.endswith(Constants.CSV_FILE_EXT):
-                    pass
+    def __init__(self):
+        self.file_name = ""
 
-    def get_data(self, data_file_path):
+    def convert_ldap_data(self, data_file_path):
+        # get the filename only
+        self.file_name = os.path.basename(data_file_path)
         data_dict = temp_dict = {}
         attrs = ["facsimileTelephoneNumber",
                  "cn",
@@ -30,19 +30,86 @@ class LdapExporter:
                     levels = line.split(":")[1].strip()
                     levels_split = levels.split(",")
                     levels_split_cleaned = list(map(lambda x: x.replace("ou=", "").replace("dc=", "").replace("cn=", ""), levels_split))
+
+                    # hint: https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary
+                    # -exercise-27.php
+                    # this will create a dictionary from a list. it checks if the combination of keys
+                    # in the list already exists inside the nested dictionary. if not it adds it
+                    # inside the nested dictionary.
+                    _data_dict = data_dict
                     for level in reversed(levels_split_cleaned):
                         try:
-                            _data_dict = data_dict[level]
+                            _data_dict = _data_dict[level]
                         except KeyError:
                             temp_dict[level] = {}
                             temp_dict = temp_dict[level]
-        print(data_dict)
+                elif any([line.startswith(attr) for attr in attrs]):
+                    key, value = line.split(":")
+                    key = key.strip()
+                    value = value.strip()
 
-    def key_exists(self, d, ks):
-        _d = d
-        for k in ks:
-            try:
-                _d = _d[k]
-            except KeyError:
-                return False
-        return True
+                    _data_dict = data_dict
+                    for level in reversed(levels_split_cleaned):
+                        _data_dict = _data_dict[level]
+                    if key in _data_dict:
+                        if not isinstance(_data_dict[key], list):
+                            _data_dict[key] = [_data_dict[key]]
+                        _data_dict[key].append(value)
+                    else:
+                        _data_dict[key] = value
+            return data_dict
+
+    def print_nested_dict(self, d):
+        # prints all the keys and values of a dictionary
+        for k, v in d.items():
+            if isinstance(v, dict):
+                yield k
+                yield from self.print_nested_dict(v)
+            else:
+                yield k, v
+
+    def export_data(self, data_dict):
+        output_path = os.path.join(Constants.OUTPUT_DIR, os.path.splitext(self.file_name)[0] + ".docx")
+        root_data_dict = data_dict['design']['ptcc']
+        doc = self.create_document()
+        for entry in self.print_nested_dict(root_data_dict):
+            if isinstance(entry, tuple):
+                key, value = entry
+                text = "{}: {}\n".format(key, value)
+                bold = False
+            else:
+                text = entry + "\n\t"
+                bold = True
+            self.add_document_text(doc, text, bold)
+        self.save_document(doc, output_path)
+
+    # document related methods (SHOULD have been a separate class)
+    def create_document(self):
+        margin = 20
+        document = Document()
+        section = document.sections[0]
+        section.page_height = Mm(297)
+        section.page_width = Mm(210)
+        section.left_margin = Mm(margin)
+        section.right_margin = Mm(margin)
+        section.top_margin = Mm(margin)
+        section.bottom_margin = Mm(margin)
+        section.header_distance = Mm(12.7)
+        section.footer_distance = Mm(12.7)
+
+        # setting the page layout to have 3 columns
+        sectPr = section._sectPr
+        cols = sectPr.xpath('./w:cols')[0]
+        cols.set(qn('w:num'), '3')
+        return document
+
+    def add_document_text(self, document, text, is_bold=False):
+        paragraph = document.add_paragraph()
+        paragraph.style = document.styles['Normal']
+        if is_bold:
+            paragraph.add_run(text).bold = True
+        else:
+            paragraph.add_run(text)
+
+    def save_document(self, document, output_path):
+        document.save(output_path)
--- python_source/LDAPExporter/test/ldap_exporter_test.py
+++ python_source/LDAPExporter/test/ldap_exporter_test.py
@@ -5,38 +5,58 @@ from src.ldap_exporter import LdapExporter
 
 
 class LdapExporterTest(unittest.TestCase):
-    def test_get_data_001(self):
-        '''
-            d = {
-                "dc=design": {
-                    "dc=ptcc": {
-                        "ou=ptcc_industry people": {
-                            "member": [
-                                {
-                                    "ou=3M Philippines": {
-                                        "member": [
-                                            {
-                                                "cn=3M Philippines": {
-                                                    "facsimileTelephoneNumber": [
-                                                        "+63 (2) 8145872",
-                                                        "+63 (2) 8420581",
-                                                        "+63 (2) 8501157"
-                                                    ],
-                                                    "telephoneNumber":[
-                                                        "+63 (2) 8133781 to 95",
-                                                        "+63 (2) 8145888"
-                                                    ]
-                                                }
-                                            }
-                                        ]
-                                    }
-                                },
-                            ]
+    def test_convert_ldap_data_001(self):
+        expected_data = {
+          'design': {
+            'ptcc': {
+              'ptcc_industry people': {
+                '3M Philippines': {
+                  '3M Philippines': {
+                    'cn': '3M Philippines',
+                    'facsimileTelephoneNumber': [
+                      '+63 (2) 8145872',
+                      '+63 (2) 8420581',
+                      '+63 (2) 8501157'
+                    ],
+                    'telephoneNumber': [
+                      '+63 (2) 8133781 to 95',
+                      '+63 (2) 8145888'
+                    ]
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        testdata = os.path.join(Constants.TEST_DIR, "testdata", "testdata001.txt")
+        le = LdapExporter()
+        actual_data = le.convert_ldap_data(testdata)
+        self.assertEqual(expected_data, actual_data)
+
+    def test_export_data_001(self):
+        data_dict = {
+            'design': {
+                'ptcc': {
+                    'ptcc_industry people': {
+                        '3M Philippines': {
+                            '3M Philippines': {
+                                'cn': '3M Philippines',
+                                'facsimileTelephoneNumber': [
+                                    '+63 (2) 8145872',
+                                    '+63 (2) 8420581',
+                                    '+63 (2) 8501157'
+                                ],
+                                'telephoneNumber': [
+                                    '+63 (2) 8133781 to 95',
+                                    '+63 (2) 8145888'
+                                ]
+                            }
                         }
                     }
                 }
             }
-        '''
-        testdata = os.path.join(Constants.TEST_DIR, "testdata", "testdata001.txt")
+        }
         le = LdapExporter()
-        le.get_data(testdata)
+        le.file_name = "testdata001.txt"
+        le.export_data(data_dict)
