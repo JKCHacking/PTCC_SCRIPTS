e3aeb397dda012c3fc8c958ea0b78032d928acbb
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-01-20 14:06:05 +0800

    [LDAPExporter] added a main.py file for iterating through the input folder.

---
+++ python_source/LDAPExporter/main.py
@@ -0,0 +1,18 @@
+import os
+from src.ldap_exporter import LdapExporter
+from src.util.constants import Constants
+
+
+def iter_input():
+    input_dir = Constants.INPUT_DIR
+    le = LdapExporter()
+    for dir_path, dir_names, file_names in os.walk(input_dir):
+        for file_name in file_names:
+            file_full_path = os.path.join(dir_path, file_name)
+            if file_full_path.endswith(Constants.TXT_FILE_EXT):
+                ldap_data_dict = le.convert_ldap_data(file_full_path)
+                le.export_data(ldap_data_dict)
+
+
+if __name__ == "__main__":
+    iter_input()
