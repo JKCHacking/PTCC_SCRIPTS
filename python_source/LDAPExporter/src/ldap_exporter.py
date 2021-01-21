8aaf309b45d3f850b75c6b51588061283f207361
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-01-21 09:48:30 +0800

    [LDAPEXPORTER] implemented handling of list type values fixed implementation for spacing and presentation in the document changed from 3 columns to 2 columns. fixed logic code for relying on ":" as separator. used the key name + ":" to be a separator. fixed implementation for going through the dictionary and adding new ones. added new tests.

--- python_source/LDAPExporter/src/ldap_exporter.py
+++ python_source/LDAPExporter/src/ldap_exporter.py
@@ -12,9 +12,8 @@ class LdapExporter:
     def convert_ldap_data(self, data_file_path):
         # get the filename only
         self.file_name = os.path.basename(data_file_path)
-        data_dict = temp_dict = {}
+        data_dict = {}
         attrs = ["facsimileTelephoneNumber",
-                 "cn",
                  "st",
                  "street",
                  "email",
@@ -25,14 +24,15 @@ class LdapExporter:
 
         with open(data_file_path, "r") as data:
             data_lines = data.readlines()
+            levels_split_cleaned = ""
             for line in data_lines:
                 if line.startswith("dn"):
                     levels = line.split(":")[1].strip()
                     levels_split = levels.split(",")
-                    levels_split_cleaned = list(map(lambda x: x.replace("ou=", "").replace("dc=", "").replace("cn=", ""), levels_split))
+                    levels_split_cleaned = list(
+                        map(lambda x: x.replace("ou=", "").replace("dc=", "").replace("cn=", "").replace("o=", ""),
+                            levels_split))
 
-                    # hint: https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary
-                    # -exercise-27.php
                     # this will create a dictionary from a list. it checks if the combination of keys
                     # in the list already exists inside the nested dictionary. if not it adds it
                     # inside the nested dictionary.
@@ -41,22 +41,25 @@ class LdapExporter:
                         try:
                             _data_dict = _data_dict[level]
                         except KeyError:
-                            temp_dict[level] = {}
-                            temp_dict = temp_dict[level]
-                elif any([line.startswith(attr) for attr in attrs]):
-                    key, value = line.split(":")
-                    key = key.strip()
-                    value = value.strip()
+                            _data_dict[level] = {}
+                            _data_dict = _data_dict[level]
 
-                    _data_dict = data_dict
-                    for level in reversed(levels_split_cleaned):
-                        _data_dict = _data_dict[level]
-                    if key in _data_dict:
-                        if not isinstance(_data_dict[key], list):
-                            _data_dict[key] = [_data_dict[key]]
-                        _data_dict[key].append(value)
-                    else:
-                        _data_dict[key] = value
+                else:
+                    for attr in attrs:
+                        if line.startswith(attr):
+                            value = line.split(attr + ":")[1]
+                            key = attr.strip()
+                            value = value.strip()
+
+                            _data_dict = data_dict
+                            for level in reversed(levels_split_cleaned):
+                                _data_dict = _data_dict[level]
+                            if key in _data_dict:
+                                if not isinstance(_data_dict[key], list):
+                                    _data_dict[key] = [_data_dict[key]]
+                                _data_dict[key].append(value)
+                            else:
+                                _data_dict[key] = value
             return data_dict
 
     def print_nested_dict(self, d):
@@ -72,15 +75,20 @@ class LdapExporter:
         output_path = os.path.join(Constants.OUTPUT_DIR, os.path.splitext(self.file_name)[0] + ".docx")
         root_data_dict = data_dict['design']['ptcc']
         doc = self.create_document()
+        par = self.add_paragraph(doc)
         for entry in self.print_nested_dict(root_data_dict):
             if isinstance(entry, tuple):
                 key, value = entry
-                text = "{}: {}\n".format(key, value)
+                if isinstance(value, list):
+                    value = "/\n".join(value)
+                    text = "{}:\n{}\n".format(key, value)
+                else:
+                    text = "{}: {}\n".format(key, value)
                 bold = False
             else:
-                text = entry + "\n\t"
+                text = "\n" + entry + "\n"
                 bold = True
-            self.add_document_text(doc, text, bold)
+            self.add_paragraph_text(par, text, bold)
         self.save_document(doc, output_path)
 
     # document related methods (SHOULD have been a separate class)
@@ -100,16 +108,19 @@ class LdapExporter:
         # setting the page layout to have 3 columns
         sectPr = section._sectPr
         cols = sectPr.xpath('./w:cols')[0]
-        cols.set(qn('w:num'), '3')
+        cols.set(qn('w:num'), '2')
         return document
 
-    def add_document_text(self, document, text, is_bold=False):
-        paragraph = document.add_paragraph()
-        paragraph.style = document.styles['Normal']
+    def add_paragraph_text(self, paragraph, text, is_bold=False):
         if is_bold:
             paragraph.add_run(text).bold = True
         else:
             paragraph.add_run(text)
+
+    def add_paragraph(self, document):
+        paragraph = document.add_paragraph()
+        paragraph.style = document.styles['Normal']
+        return paragraph
 
     def save_document(self, document, output_path):
         document.save(output_path)
--- python_source/LDAPExporter/test/ldap_exporter_test.py
+++ python_source/LDAPExporter/test/ldap_exporter_test.py
@@ -12,7 +12,6 @@ class LdapExporterTest(unittest.TestCase):
               'ptcc_industry people': {
                 '3M Philippines': {
                   '3M Philippines': {
-                    'cn': '3M Philippines',
                     'facsimileTelephoneNumber': [
                       '+63 (2) 8145872',
                       '+63 (2) 8420581',
@@ -34,6 +33,50 @@ class LdapExporterTest(unittest.TestCase):
         actual_data = le.convert_ldap_data(testdata)
         self.assertEqual(expected_data, actual_data)
 
+    def test_convert_ldap_data_002(self):
+        expected_data = \
+        {
+          "design": {
+            "ptcc": {
+              "ptcc_industry people": {
+                "08DB ACOUSTIC ENVIRONMENTS": {
+                  "facsimileTelephoneNumber": "+63 2 718-0088",
+                  "registeredAddress": "23Floor Unit B Summit One Tower, 530 Shaw Blvd., Mandaluyong City, 1552 Metro Manila, Philippines",
+                  "telephoneNumber": "+63 2 718-0808",
+                  "Joseph N. Tan": {
+                    "facsimileTelephoneNumber": [
+                      "Telefax: +63 2 533-0808",
+                      "+63 2 718-0088"
+                    ],
+                    "mail": "info@zeroeightdb.com",
+                    "mobile": "+63 917 891-8471",
+                    "registeredAddress": "Unit-B 23F Summit One Tower, 530 Shaw Blvd., Mandaluyong City 1552, Metro Manila, Philippines",
+                    "telephoneNumber": [
+                      "Direct: +63 2 531-0808",
+                      "Trunk: +63 2 718-0808"
+                    ]
+                  },
+                  "George Lee": {
+                    "facsimileTelephoneNumber": "+63 2 718-0088",
+                    "mail": [
+                      "zeroeightdb@yahoo.com",
+                      "admin@zeroeightdb.com"
+                    ],
+                    "mobile": "+63 915 880-0088",
+                    "registeredAddress": "Unit-B 23F Summit One Tower, 530 Shaw Blvd., Mandaluyong City 1552, Metro Manila, Philippines",
+                    "telephoneNumber": "+63 2 718-0808"
+                  }
+                }
+              }
+            }
+          }
+        }
+
+        testdata = os.path.join(Constants.TEST_DIR, "testdata", "testdata003.txt")
+        le = LdapExporter()
+        actual_data = le.convert_ldap_data(testdata)
+        self.assertEqual(expected_data, actual_data)
+
     def test_export_data_001(self):
         data_dict = {
             'design': {
@@ -41,7 +84,6 @@ class LdapExporterTest(unittest.TestCase):
                     'ptcc_industry people': {
                         '3M Philippines': {
                             '3M Philippines': {
-                                'cn': '3M Philippines',
                                 'facsimileTelephoneNumber': [
                                     '+63 (2) 8145872',
                                     '+63 (2) 8420581',
@@ -58,5 +100,48 @@ class LdapExporterTest(unittest.TestCase):
             }
         }
         le = LdapExporter()
-        le.file_name = "testdata001.txt"
+        le.file_name = "testoutput001.txt"
+        le.export_data(data_dict)
+
+    def test_export_data_002(self):
+        data_dict = \
+            {
+                "design": {
+                    "ptcc": {
+                        "ptcc_industry people": {
+                            "08DB ACOUSTIC ENVIRONMENTS": {
+                                "facsimileTelephoneNumber": "+63 2 718-0088",
+                                "registeredAddress": "23Floor Unit B Summit One Tower, 530 Shaw Blvd., Mandaluyong City, 1552 Metro Manila, Philippines",
+                                "telephoneNumber": "+63 2 718-0808",
+                                "Joseph N. Tan": {
+                                    "facsimileTelephoneNumber": [
+                                        "Telefax: +63 2 533-0808",
+                                        "+63 2 718-0088"
+                                    ],
+                                    "mail": "info@zeroeightdb.com",
+                                    "mobile": "+63 917 891-8471",
+                                    "registeredAddress": "Unit-B 23F Summit One Tower, 530 Shaw Blvd., Mandaluyong City 1552, Metro Manila, Philippines",
+                                    "telephoneNumber": [
+                                        "Direct: +63 2 531-0808",
+                                        "Trunk: +63 2 718-0808"
+                                    ]
+                                },
+                                "George Lee": {
+                                    "facsimileTelephoneNumber": "+63 2 718-0088",
+                                    "mail": [
+                                        "zeroeightdb@yahoo.com",
+                                        "admin@zeroeightdb.com"
+                                    ],
+                                    "mobile": "+63 915 880-0088",
+                                    "registeredAddress": "Unit-B 23F Summit One Tower, 530 Shaw Blvd., Mandaluyong City 1552, Metro Manila, Philippines",
+                                    "telephoneNumber": "+63 2 718-0808"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        le = LdapExporter()
+        le.file_name = "testoutput002.txt"
         le.export_data(data_dict)
