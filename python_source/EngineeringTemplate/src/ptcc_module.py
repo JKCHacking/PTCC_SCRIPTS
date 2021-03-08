49f89980ec5978caf3bf5be8ac2d1ce645185771
Joshnee Kim Cunanan <joshnee.kim.cunanan@gmail.com>
2021-03-08 15:13:29 +0800

    [JupyterEngineeringTemplate] added implementation for inline result and rounding of number of decimal

--- python_source/EngineeringTemplate/src/ptcc_module.py
+++ python_source/EngineeringTemplate/src/ptcc_module.py
@@ -1,5 +1,5 @@
 from sympy import *
-from IPython.display import *
+from IPython.display import display, Latex, Markdown
 import pint
 
 
@@ -7,7 +7,7 @@ UNIT = pint.UnitRegistry()
 UNIT.default_system = 'SI'   # setting default unit system
                              # choices are ['Planck', 'SI', 'US',
                              # 'atomic', 'cgs', 'imperial', 'mks']
-LATEX_CONTENT = ["\\begin{align}", "\\end{align}"]
+# LATEX_CONTENT = ["\\begin{align}", "\\end{align}"]
 
 
 class EquationWriter:
@@ -17,7 +17,8 @@ class EquationWriter:
         self.font_size = font_size
         self.equation_namespace = {}
 
-    def define(self, equation, evaluate=False, decimal=2, inline=False):
+    def define(self, equation, evaluate=False, num_decimal=2, inline=False):
+        out_latex = ""
         # annotation, pref_units, decimals, printout, inline
         left_expr, right_expr = equation.split("=")
         left_expr = left_expr.strip()
@@ -27,27 +28,38 @@ class EquationWriter:
 
         # convert sympy equation to latex equation.
         eq_latex = self.__convert_to_latex(sym_eq)
-        print(eq_latex)
 
         if evaluate:
-            res_eq = self.__evaluate(sym_eq, decimal)
+            res_eq = self.__evaluate(sym_eq, num_decimal)
             # update the equation namespace
             self.__add_eq_to_namespace(res_eq)
             res_eq_latex = self.__convert_to_latex(res_eq)
-            print(res_eq_latex)
+            if inline:
+                res_eq = res_eq_latex.split("$")[1]
+                eq = eq_latex.split("$")[1]
+                res_eq_rhs = res_eq.split("=")[1].strip()
+                out_latex = "${} = {}$".format(eq, res_eq_rhs)
+            else:
+                out_latex = "{}<br>{}".format(eq_latex, res_eq_latex)
         else:
             # add the equation to the namespace
             self.__add_eq_to_namespace(sym_eq)
+            out_latex = eq_latex
+        display(Markdown(out_latex))
 
-    def __evaluate(self, equation, decimal):
+    def __evaluate(self, equation, num_decimal):
         # get only the needed variables for substitution
         var_list = list(equation.rhs.atoms(Symbol))
         var_sub_dict = {}
         for sym_var in var_list:
             sym_var_str = str(sym_var)
             var_sub_dict.update({sym_var_str: self.equation_namespace[sym_var_str]})
-        res = equation.subs(var_sub_dict).evalf(decimal)
+        res = equation.subs(var_sub_dict).evalf()
+        res = self.__round_expr(res, num_decimal)
         return res
+
+    def __round_expr(self, expr, num_digits):
+        return expr.xreplace({n: round(n, num_digits) for n in expr.atoms(Number)})
 
     def __convert_to_latex(self, s):
         '''
@@ -61,7 +73,6 @@ class EquationWriter:
         =======
             res_latex:latex - equation in latex
         '''
-        # embed in $ for math mode compilation.
         res_latex = "${}$".format(latex(s, mul_symbol='dot'))
         return res_latex
 
