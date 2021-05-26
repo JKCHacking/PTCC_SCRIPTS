e60430a44b41c91d10ba1e67e8e398263ff65e5d
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-05-26 14:15:45 +0800

    [WebAutomation(selenium)] fixed the skipping of pdf file proocessing and added a delay.added the transferring of the pdf file from default landing directory to its subdirectory making its location more specific to the ASTM website.

--- python_source/WebAutomationSelenium/src/astm_pdf_automation.py
+++ python_source/WebAutomationSelenium/src/astm_pdf_automation.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import time
 import re
 from selenium.webdriver import Firefox
@@ -36,11 +37,11 @@ class Astm:
         self.main_page_url = "http://astm/"
         self.sub_directories = [
             "A.htm",
-            "B.htm",
-            "C.htm",
-            "D.htm",
-            "E.htm",
-            "F.htm",
+            # "B.htm",
+            # "C.htm",
+            # "D.htm",
+            # "E.htm",
+            # "F.htm",
             # "G.htm"
         ]
 
@@ -51,12 +52,10 @@ class Astm:
             yield sub_dir.split(".")[0]
 
     def get_standard_links(self):
-        standard_link_list = []
         links = self.browser.find_elements_by_class_name("bluenolinelinks")
         for link in links:
             if link.get_attribute("innerHTML").startswith("Standard"):
-                standard_link_list.append(link)
-        return standard_link_list
+                yield link
 
 
 class DownloadMngr:
@@ -77,8 +76,9 @@ class DownloadMngr:
                     self.browser.close()
                     self.browser.switch_to.window(self.browser.window_handles[0])
                     return file_name
-            except Exception:
-                pass
+            except Exception as e:
+                print(str(e))
+
             time.sleep(1)
             if time.time() > end_time:
                 return None
@@ -90,23 +90,30 @@ def main():
     dl_mngr = DownloadMngr(browser)
     astm = Astm(browser)
     for letter in astm.goto_subdirectories():
-        standard_links = astm.get_standard_links()
-        for link in standard_links:
+        for link in astm.get_standard_links():
             link.click()
-            # wait 3 seconds
-            file_name = dl_mngr.get_downloaded_file_name(3)
-            fp_file_name = os.path.join(Constants.OUTPUT_DIR, file_name)
-            if os.path.exists(fp_file_name):
-                name_of_standard = link.get_attribute("innerHTML")
-                name_of_standard = re.sub(r"[\\/:*?\"<>|]", '', name_of_standard)
-                new_fp_file_name = os.path.join(Constants.OUTPUT_DIR, letter,
-                                                "{}.pdf".format(name_of_standard))
-                time.sleep(1)
-                try:
-                    os.rename(fp_file_name, new_fp_file_name)
-                    os.remove(fp_file_name)
-                except OSError:
-                    pass
+            time.sleep(1)
+            # wait 10 seconds
+            file_name = dl_mngr.get_downloaded_file_name(10)
+            if file_name:
+                fp_file_name = os.path.join(Constants.OUTPUT_DIR, file_name)
+                subdir_fp_file_name = os.path.join(Constants.OUTPUT_DIR, letter, file_name)
+                if os.path.exists(fp_file_name):
+                    name_of_standard = link.get_attribute("innerHTML")
+                    # remove invalid symbols for windows file name
+                    name_of_standard = re.sub(r"[\\/:*?\"<>|]", '', name_of_standard)
+                    new_fp_file_name = os.path.join(Constants.OUTPUT_DIR, letter,
+                                                    "{}.pdf".format(name_of_standard))
+                    try:
+                        # transfer downloaded file to its corresponding sub-directory letter
+                        os.rename(fp_file_name, subdir_fp_file_name)
+                        time.sleep(1)
+                        # rename the donwloaded file to its correct standard name
+                        os.rename(subdir_fp_file_name, new_fp_file_name)
+                    except Exception as e:
+                        print(str(e))
+            else:
+                print("File name is None ", link.get_attribute("innerHTML"))
 
 
 if __name__ == "__main__":
