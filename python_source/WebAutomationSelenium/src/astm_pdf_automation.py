189fa93da2f82c9b86f1b20645b2369f52e4bbf9
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-05-27 09:35:41 +0800

    [WebAutomation(selenium)] implemented a better algorithm, by first downloading all pdfs then renaming. it prevented sleep/pausing making it a faster implementation.

--- python_source/WebAutomationSelenium/src/astm_pdf_automation.py
+++ python_source/WebAutomationSelenium/src/astm_pdf_automation.py
@@ -36,13 +36,13 @@ class Astm:
         self.browser = browser
         self.main_page_url = "http://astm/"
         self.sub_directories = [
-            "A.htm",
+            # "A.htm",
             # "B.htm",
             # "C.htm",
             # "D.htm",
             # "E.htm",
             # "F.htm",
-            # "G.htm"
+            "G.htm"
         ]
 
     def goto_subdirectories(self):
@@ -84,36 +84,54 @@ class DownloadMngr:
                 return None
 
 
+def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    to_print = f'\r{prefix} |{bar}| ({iteration}/{total}) {percent}% {suffix}'
+    print(to_print, end="", flush=True)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
 def main():
     ff_factory = FirefoxBrowserFactory()
     browser = ff_factory.create_browser()
     dl_mngr = DownloadMngr(browser)
     astm = Astm(browser)
     for letter in astm.goto_subdirectories():
+        astm_file_name_pairs = []
+        # download all the PDF then save some data.
+        print("Downloading PDFs in Group {}...".format(letter))
         for link in astm.get_standard_links():
             link.click()
-            time.sleep(1)
+            # time.sleep(3)
             # wait 10 seconds
-            file_name = dl_mngr.get_downloaded_file_name(10)
-            if file_name:
-                fp_file_name = os.path.join(Constants.OUTPUT_DIR, file_name)
-                subdir_fp_file_name = os.path.join(Constants.OUTPUT_DIR, letter, file_name)
-                if os.path.exists(fp_file_name):
-                    name_of_standard = link.get_attribute("innerHTML")
-                    # remove invalid symbols for windows file name
-                    name_of_standard = re.sub(r"[\\/:*?\"<>|]", '', name_of_standard)
-                    new_fp_file_name = os.path.join(Constants.OUTPUT_DIR, letter,
-                                                    "{}.pdf".format(name_of_standard))
-                    try:
-                        # transfer downloaded file to its corresponding sub-directory letter
-                        os.rename(fp_file_name, subdir_fp_file_name)
-                        time.sleep(1)
-                        # rename the donwloaded file to its correct standard name
-                        os.rename(subdir_fp_file_name, new_fp_file_name)
-                    except Exception as e:
-                        print(str(e))
-            else:
-                print("File name is None ", link.get_attribute("innerHTML"))
+            file_name = dl_mngr.get_downloaded_file_name(5)
+            name_of_standard = "{}.pdf".format(link.get_attribute("innerHTML"))
+            # remove invalid symbols to make a valid windows file name
+            name_of_standard = re.sub(r"[\\/:*?\"<>|]", '', name_of_standard)
+            if file_name and name_of_standard:
+                astm_file_name_pairs.append({"old_file_name": file_name, "new_file_name": name_of_standard})
+
+        total_pairs = len(astm_file_name_pairs)
+        print_progress_bar(0, total_pairs,
+                           prefix="Renaming PDFs in Group {}".format(letter),
+                           suffix="Completed",
+                           length=50)
+        for i, file_name_pair in enumerate(astm_file_name_pairs):
+            old_file_name = file_name_pair["old_file_name"]
+            new_file_name = file_name_pair["new_file_name"]
+            fp_old_file_name = os.path.join(Constants.OUTPUT_DIR, old_file_name)
+            fp_new_file_name = os.path.join(Constants.OUTPUT_DIR, letter, new_file_name)
+
+            if os.path.exists(fp_old_file_name):
+                os.rename(fp_old_file_name, fp_new_file_name)
+            print_progress_bar(i + 1, total_pairs,
+                               prefix="Renaming PDFs in Group {}".format(letter),
+                               suffix="Completed",
+                               length=50)
 
 
 if __name__ == "__main__":
