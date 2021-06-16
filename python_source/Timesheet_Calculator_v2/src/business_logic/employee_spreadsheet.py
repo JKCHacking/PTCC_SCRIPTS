28dd1f62b2bce11aa49c43ac1da6e0cfc270c366
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-06-16 09:41:12 +0800

    [TimesheetCalculatorV2] Changes: * implemented applying bold and borders in cells. * updated testdata

--- python_source/Timesheet_Calculator_v2/src/business_logic/employee_spreadsheet.py
+++ python_source/Timesheet_Calculator_v2/src/business_logic/employee_spreadsheet.py
@@ -1,5 +1,8 @@
 import datetime
 from src.business_logic.calculator import Calculator
+from openpyxl.styles.borders import Border, Side
+from openpyxl.styles.fonts import Font
+from openpyxl.cell.cell import Cell
 
 
 class EmployeeSpreadsheet:
@@ -18,6 +21,7 @@ class EmployeeSpreadsheet:
         self.ws["A2"] = "PERIOD COVERED: {} to {}".format(self.ts_workbook.get_start_date().strftime("%d-%b-%Y"),
                                                           self.ts_workbook.get_end_date().strftime("%d-%b-%Y"))
         self.ws["A3"] = self.employee_full_name
+        self.ws["A3"].font = Font(size=12, bold=True, name='Calibri')
 
     def write_body(self):
         days_dict = {
@@ -35,8 +39,8 @@ class EmployeeSpreadsheet:
             first_day = week[0]
             last_day = week[-1]
             self.ws.append([""])
-            self.ws.append(["WEEK COVERED: {} to {}".format(first_day.strftime("%d-%b-%Y"),
-                                                            last_day.strftime("%d-%b-%Y"))])
+            self.ws.append(self.__apply_bold_cells(["WEEK COVERED: {} to {}".format(first_day.strftime("%d-%b-%Y"),
+                                                                                    last_day.strftime("%d-%b-%Y"))]))
             self.__write_employee_report_columns(self.ws)
             for day in week:
                 # if holiday, treat all hours as excess
@@ -49,14 +53,31 @@ class EmployeeSpreadsheet:
                 day_name = days_dict[day.weekday()]
                 tito = self.__get_tito_of_the_day(day, self.tasks)
                 total_mins = self.__get_total_minutes_of_the_day(day, self.tasks)
-
                 excess_minutes = 0
                 if total_mins > credited_minutes:
                     excess_minutes = total_mins - credited_minutes
                 if tito != "":
-                    self.ws.append([date_string, day_name, tito, total_mins, credited_minutes, excess_minutes])
+                    data = [date_string, day_name, tito, total_mins, credited_minutes, excess_minutes]
                 else:
-                    self.ws.append([date_string, day_name, tito, total_mins, 0, excess_minutes])
+                    data = [date_string, day_name, tito, total_mins, 0, excess_minutes]
+                self.ws.append(self.__apply_border_cells(data))
+
+    def __apply_border_cells(self, data):
+        for value in data:
+            c = Cell(self.ws, column="A", row=1, value=value)
+            thin_border = Border(left=Side(style="thin"),
+                                 right=Side(style="thin"),
+                                 top=Side(style="thin"),
+                                 bottom=Side(style="thin"))
+            c.border = thin_border
+            yield c
+
+    def __apply_bold_cells(self, data):
+        for value in data:
+            c = Cell(self.ws, column="A", row=1, value=value)
+            font = Font(bold=True, name='Calibri')
+            c.font = font
+            yield c
 
     def __get_tito_of_the_day(self, date, tasks):
         tito_list = []
@@ -79,8 +100,9 @@ class EmployeeSpreadsheet:
         return total_minutes
 
     def __write_employee_report_columns(self, ws):
-        ws.append(["Date", "Day", "Time-in/Time-out", "Rendered MINS for the Day", "Credited Regular Log [480 = 1 day]",
-                   "Minutes in excess of 480; Sat/Sun Duties"])
+        data = ["Date", "Day", "Time-in/Time-out", "Rendered MINS for the Day", "Credited Regular Log [480 = 1 day]",
+                "Minutes in excess of 480; Sat/Sun Duties"]
+        ws.append(self.__apply_border_cells(data))
 
     def __generate_mon_to_sun_dates(self):
         week = []
--- python_source/Timesheet_Calculator_v2/src/business_logic/summary_spreadsheet.py
+++ python_source/Timesheet_Calculator_v2/src/business_logic/summary_spreadsheet.py
@@ -1,5 +1,8 @@
 import datetime
 from src.business_logic.calculator import Calculator
+from openpyxl.styles.borders import Border, Side
+from openpyxl.styles.fonts import Font
+from openpyxl.cell.cell import Cell
 
 
 class SummarySpreadsheet:
@@ -13,16 +16,14 @@ class SummarySpreadsheet:
             self.ts_workbook.get_end_date().strftime("%d-%b-%Y"))
         self.ws["A2"] = datetime.datetime.now().strftime("%d-%B-%Y")
         self.ws.append([""])
-        self.ws.append(["NAME", "PROJECT", "TIME SPENT"])
+        self.ws.append(self.__apply_border_cells(["NAME", "PROJECT", "TIME SPENT"]))
 
     def write_body(self, employee, tasks):
-        name = "{}, {} {}.".format(employee.get_last_name(),
-                                   employee.get_first_name(),
-                                   employee.get_middle_name()[0])
+        name = "{}, {} {}.".format(employee.get_last_name(),employee.get_first_name(),employee.get_middle_name()[0])
         projects = self.__get_projects(tasks)
         projects = "\n".join(sorted(projects))
         time_spent = self.__get_total_hours(tasks)
-        self.ws.append([name, projects, time_spent])
+        self.ws.append(self.__apply_border_cells([name, projects, time_spent]))
 
     def write_footers(self):
         self.ws.append([""])
@@ -50,3 +51,20 @@ class SummarySpreadsheet:
             if self.ts_workbook.get_start_date() <= task.get_date() <= self.ts_workbook.get_end_date():
                 total_hours += calculator.calculate_task_hours(task.get_date(), task.get_time_in(), task.get_time_out())
         return total_hours
+
+    def __apply_border_cells(self, data):
+        for value in data:
+            c = Cell(self.ws, column="A", row=1, value=value)
+            thin_border = Border(left=Side(style="thin"),
+                                 right=Side(style="thin"),
+                                 top=Side(style="thin"),
+                                 bottom=Side(style="thin"))
+            c.border = thin_border
+            yield c
+
+    def __apply_bold_cells(self, data):
+        for value in data:
+            c = Cell(self.ws, column="A", row=1, value=value)
+            font = Font(bold=True, name='Calibri')
+            c.font = font
+            yield c
