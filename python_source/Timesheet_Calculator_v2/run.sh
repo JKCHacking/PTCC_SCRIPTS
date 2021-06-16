accd038f8df9cb953c8935b734a5e86aa7df3155
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-06-16 16:27:14 +0800

    [TimesheetCalculatorV2] Changes: * added try catch implementation in run.sh to run either python or python3.6

--- python_source/Timesheet_Calculator_v2/run.sh
+++ python_source/Timesheet_Calculator_v2/run.sh
@@ -2,4 +2,8 @@
 # shellcheck disable=SC2164
 printf "Starting Timesheet Calculator..."
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-python "$parent_path"/main.py
+{
+  python "$parent_path"/main.py
+  } || {
+    python3.6 "$parent_path"/main.py
+}
