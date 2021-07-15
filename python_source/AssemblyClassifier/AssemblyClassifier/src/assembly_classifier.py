cd5a6bba565fb61a59efbd14b7e9290ecc7c2ac2
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-07-15 14:15:44 +0800

    [AssemblyClassifier] added a new script implementation for grouping all part of an assembly in 1 folder from a pool of unclassified parts and assembly files.

---
+++ python_source/AssemblyClassifier/AssemblyClassifier/src/assembly_classifier.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+from comtypes import client
+
+
+class AssemblyClassifier:
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+        # initializing the Inventor application
+        inv_progid = "Inventor.Application"
+        try:
+            self.inv_app = client.GetActiveObject(inv_progid, dynamic=True)
+        except OSError:
+            self.inv_app = client.CreateObject(inv_progid, dynamic=True)
+
+    def collect_parts(self, assembly_path):
+        assembly = self.inv_app.Documents.Open(assembly_path)
+        component_def = assembly.ComponentDefinition
+        assembly_folder_name = os.path.splitext(os.path.basename(assembly_path))[0]
+
+        for occurrence in component_def.Occurrences:
+            ref_doc_desc = occurrence.ReferencedDocumentDescriptor
+            if ref_doc_desc:
+                part_path = ref_doc_desc.FullDocumentName
+                self.__copy_to_assembly_folder(assembly_folder_name, part_path)
+        assembly.Close(True)
+        self.__copy_to_assembly_folder(assembly_folder_name, assembly_path)
+
+    def __copy_to_assembly_folder(self, assembly_folder_name, src):
+        landing_folder = os.path.join(self.output_dir, assembly_folder_name)
+        if not os.path.exists(landing_folder):
+            os.mkdir(landing_folder)
+        dest = os.path.join(landing_folder, os.path.basename(src))
+        shutil.copyfile(src, dest)
---
+++ python_source/AssemblyClassifier/AssemblyClassifier/src/main.py
@@ -0,0 +1,16 @@
+import os
+from assembly_classifier import AssemblyClassifier
+from constants import Constants
+
+
+def main():
+    asm_classifier = AssemblyClassifier(Constants.OUTPUT_DIR)
+    for dir_path, dir_names, file_names in os.walk(Constants.INPUT_DIR):
+        for file_name in file_names:
+            if file_name.endswith(".iam"):
+                asm_full_path = os.path.join(dir_path, file_name)
+                asm_classifier.collect_parts(asm_full_path)
+
+
+if __name__ == "__main__":
+    main()
