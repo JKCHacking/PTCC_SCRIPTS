02be94b8aea63c4ac7b116a584e4f3b6b0a1faf6
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-09-21 15:17:15 +0800

    [RhinoScripts] added implementation for searching nearest text, exporting to stp file, getting to orient object with respect to the top view plane. added tests

--- python_source/RhinoScripts/src/parts_to_stp.py
+++ python_source/RhinoScripts/src/parts_to_stp.py
@@ -1,4 +1,5 @@
 import rhinoscriptsyntax as rs
+import Rhino.Geometry as rg
 
 
 def get_bounding_box(obj_id):
@@ -25,12 +26,70 @@ def get_bounding_box(obj_id):
     return bb_points
 
 
+def orient_to_top(part_id, misc_ids):
+    bb_points = get_bounding_box(part_id)
+    box = rs.AddBox(bb_points)
+    surfaces = rs.ExplodePolysurfaces(box)
+    areas = []
+    for surface in surfaces:
+        areas.append(rs.SurfaceArea(surface))
+    lying_plane = surfaces[areas.index(max(areas))]
+    rs.Command("CPlane _O _SelID {}".format(lying_plane), echo=False)
+    curr_cplane = rs.ViewCPlane()
+    top_plane = rs.ViewCPlane("Top")
+    xform = rg.Transform.PlaneToPlane(curr_cplane, top_plane)
+    new_obj_id = rs.TransformObject(part_id, matrix=xform, copy=True)
+    new_misc_ids = []
+    for obj_id in misc_ids:
+        new_misc_id = rs.TransformObject(obj_id, matrix=xform, copy=True)
+        new_misc_ids.append(new_misc_id)
+    rs.DeleteObjects(surfaces)
+    rs.DeleteObject(box)
+    return new_obj_id, new_misc_ids
+
+
+def export_to_stp(stp_filename, obj_ids):
+    rs.SelectObjects(obj_ids)
+    rs.Command("_-Export {} _Enter".format(stp_filename), False)
+    rs.UnSelectObjects(obj_ids)
+
+
+def get_specific_part_name(obj_id):
+    layer = rs.ObjectLayer(obj_id)
+    spec_pname = layer.split("::")[-1].split("...")[0].strip()
+    return spec_pname
+
+
+def search_nearest_text(part_id):
+    position_list = get_specific_part_name(part_id).split("-")[1:]
+    position = " ".join(position_list)
+
+    all_obj_ids = rs.AllObjects()
+    txt_ids = []
+    for obj_id in all_obj_ids:
+        if rs.IsText(obj_id) and rs.TextObjectText(obj_id) == position:
+            txt_ids.append(obj_id)
+    part_cent_pt = rs.SurfaceVolumeCentroid(part_id)[0]
+    txt_pt_list = [rs.TextObjectPoint(txt_id) for txt_id in txt_ids]
+    pt_ids = rs.AddPoints(txt_pt_list)
+    closest_pt_id, pt_obj = rs.PointClosestObject(part_cent_pt, pt_ids)
+    txt_id = txt_ids[txt_pt_list.index(pt_obj)]
+    rs.DeleteObjects(pt_ids)
+    return txt_id
+
+
 def main():
     all_obj_ids = rs.AllObjects()
     for obj_id in all_obj_ids:
-        # get the bounding box
-        bb_points = get_bounding_box(obj_id)
-        rs.AddPoints(bb_points)
+        if rs.IsPolysurface(obj_id):
+            txt_id = search_nearest_text(obj_id)
+            or_part_id, or_txt_id = orient_to_top(obj_id, txt_id)
+            curve_ids = rs.ExplodeText(or_txt_id)
+            ids_to_export = curve_ids.append(or_part_id)
+            filename = "{}.stp".format(get_specific_part_name(or_part_id))
+            export_to_stp(filename, ids_to_export)
+            rs.DeleteObject(or_part_id)
+            rs.DeleteObject(or_txt_id)
 
 
 if __name__ == "__main__":
--- python_source/RhinoScripts/test/parts_to_stp_test.py
+++ python_source/RhinoScripts/test/parts_to_stp_test.py
@@ -3,19 +3,44 @@ import sys
 import os
 sys.path.append("{}/../src".format(os.path.dirname(os.path.realpath(__file__))))
 import parts_to_stp
+reload(parts_to_stp)
 import rhinoscriptsyntax as rs
 
 
 class PartsToStpTest(unittest.TestCase):
-    def test_bounding_box(self):
-        top_chord_id = rs.ObjectsByName("1421-H01-F01-CP01")
-        bb_points = parts_to_stp.get_bounding_box(top_chord_id)
-        rs.AddPoints(bb_points)
-        for bb_pt in bb_points:
-            print(bb_pt)
+    def test_get_lying_plane(self):
+        part_id = rs.ObjectsByName("1421-STC01-500,00")[0]
+        text_id = rs.ObjectsByName("TextTest1")[0]
+        parts_to_stp.orient_to_top(part_id, text_id)
+
+        part_id = rs.ObjectsByName("PartTest2")[0]
+        text_id = rs.ObjectsByName("TextTest2")[0]
+        parts_to_stp.orient_to_top(part_id, text_id)
+
+    def test_search_pair_text(self):
+        obj_id = rs.ObjectsByName("PartTest1")[0]
+        txt_id = parts_to_stp.search_nearest_text(obj_id)
+        rs.SelectObject(txt_id)
+        self.assertEqual(rs.ObjectsByName("TextTest1")[0], txt_id)
+
+        obj_id = rs.ObjectsByName("PartTest2")[0]
+        txt_id = parts_to_stp.search_nearest_text(obj_id)
+        rs.SelectObject(txt_id)
+        self.assertEqual(rs.ObjectsByName("TextTest2")[0], txt_id)
+
+    def test_export_to_stp(self):
+        test_filename = "test_stp.stp"
+        part_id = rs.ObjectsByName("PartTest3")[0]
+        text_id = rs.ObjectsByName("TextTest3")[0]
+        curve_ids = rs.ExplodeText(text_id)
+        objs_to_export = [curve_id for curve_id in curve_ids]
+        objs_to_export.append(part_id)
+        parts_to_stp.export_to_stp(test_filename, objs_to_export)
 
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    suite.addTest(PartsToStpTest("test_bounding_box"))
+    # suite.addTest(PartsToStpTest("test_get_lying_plane"))
+    # suite.addTest(PartsToStpTest("test_search_pair_text"))
+    suite.addTest(PartsToStpTest("test_export_to_stp"))
     unittest.TextTestRunner(verbosity=2).run(suite)
