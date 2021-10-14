d6f5bb10dfff47ab1302b3c4291f1b58cfa7acf3
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-10-14 11:24:48 +0800

    [RHINOSCRITPS] updated implementation: * modified implementation for orient_to_top to have separate function. getting lying plane xform and transforming object. * added the implementation to include other text (except for engraving text) in searching for objects. * added implementation to include threads in searching for obejcts * modified main to have only 1 option to use the script.

--- python_source/RhinoScripts/src/parts_to_stp.py
+++ python_source/RhinoScripts/src/parts_to_stp.py
@@ -1,8 +1,10 @@
+import os
+import datetime
 import rhinoscriptsyntax as rs
 import Rhino.Geometry as rg
 
 
-def get_bounding_box(obj_id):
+def get_min_bb(obj_id):
     bb_points = []
     bb_list = []
     if rs.IsPolysurface(obj_id):
@@ -26,8 +28,7 @@ def get_bounding_box(obj_id):
     return bb_points
 
 
-def orient_to_top(part_id, misc_ids):
-    bb_points = get_bounding_box(part_id)
+def get_lying_plane_xform(bb_points):
     box = rs.AddBox(bb_points)
     surfaces = rs.ExplodePolysurfaces(box)
     areas = []
@@ -38,14 +39,16 @@ def orient_to_top(part_id, misc_ids):
     curr_cplane = rs.ViewCPlane()
     top_plane = rs.ViewCPlane("Top")
     xform = rg.Transform.PlaneToPlane(curr_cplane, top_plane)
-    new_obj_id = rs.TransformObject(part_id, matrix=xform, copy=True)
-    new_misc_ids = []
-    for obj_id in misc_ids:
-        new_misc_id = rs.TransformObject(obj_id, matrix=xform, copy=True)
-        new_misc_ids.append(new_misc_id)
-    rs.DeleteObjects(surfaces)
     rs.DeleteObject(box)
-    return new_obj_id, new_misc_ids
+    rs.DeleteObjects(surfaces)
+    return xform
+
+
+def tranform_objects(obj_ids, xform):
+    xform_obj_ids = []
+    for obj_id in obj_ids:
+        xform_obj_ids.append(rs.TransformObject(obj_id, matrix=xform, copy=True))
+    return xform_obj_ids
 
 
 def export_to_stp(stp_filename, obj_ids):
@@ -60,7 +63,7 @@ def get_specific_part_name(obj_id):
     return spec_pname
 
 
-def search_nearest_text(part_id):
+def get_engravings(part_id):
     position_list = get_specific_part_name(part_id).split("-")[1:]
     position = " ".join(position_list)
     all_obj_ids = rs.AllObjects()
@@ -68,69 +71,91 @@ def search_nearest_text(part_id):
     for obj_id in all_obj_ids:
         if rs.IsText(obj_id) and rs.TextObjectText(obj_id) == position:
             txt_ids.append(obj_id)
-    part_cent_pt = rs.SurfaceVolumeCentroid(part_id)[0]
-    txt_pt_list = [rs.TextObjectPoint(txt_id) for txt_id in txt_ids]
-    pt_ids = rs.AddPoints(txt_pt_list)
-    closest_pt_id, pt_obj = rs.PointClosestObject(part_cent_pt, pt_ids)
-    txt_id = txt_ids[txt_pt_list.index(pt_obj)]
-    rs.DeleteObjects(pt_ids)
+    if txt_ids:
+        part_cent_pt = rs.SurfaceVolumeCentroid(part_id)[0]
+        txt_pt_list = [rs.TextObjectPoint(txt_id) for txt_id in txt_ids]
+        pt_ids = rs.AddPoints(txt_pt_list)
+        closest_pt_id, pt_obj = rs.PointClosestObject(part_cent_pt, pt_ids)
+        txt_id = txt_ids[txt_pt_list.index(pt_obj)]
+        rs.DeleteObjects(pt_ids)
+    else:
+        txt_id = None
     return txt_id
 
 
-def get_multiparts(holoplot_num):
-    all_obj_ids = rs.GetObjects("Select parts to export stp")
+def get_other_texts(bb_points):
+    other_text_ids = []
+    all_obj_ids = rs.AllObjects()
     for obj_id in all_obj_ids:
+        obj_name = rs.ObjectLayer(obj_id).split("::")[-1]
+        if rs.IsText(obj_id) and rs.TextObjectText(obj_id) not in obj_name and rs.IsObjectInBox(obj_id, bb_points):
+            other_text_ids.append(obj_id)
+    return other_text_ids
+
+
+def get_threads(bb_points):
+    thread_ids = []
+    # find all threads that's inside the part
+    all_obj_ids = rs.AllObjects()
+    for obj_id in all_obj_ids:
+        obj_name = rs.ObjectLayer(obj_id).split("::")[-1]
+        if rs.IsPolysurface(obj_id) and "threaded" in obj_name and rs.IsObjectInBox(obj_id, bb_points):
+            thread_ids.append(obj_id)
+    return thread_ids
+
+
+def convert_parts_to_stp(holoplot_num, sel_obj_ids):
+    for obj_id in sel_obj_ids:
         if rs.IsPolysurface(obj_id):
-            print("Working on: {}".format(get_specific_part_name(obj_id)))
-            ids_to_export = []
-            full_path = "H:\\Desktop\\projects\\holoplot\\{}\\{}"
-            txt_id = search_nearest_text(obj_id)
-            misc_ids = [txt_id]
-            or_part_id, or_misc_ids = orient_to_top(obj_id, misc_ids)
-            or_txt_id = or_misc_ids[0]
-            curve_ids = rs.ExplodeText(or_txt_id)
-            ids_to_export.append(or_part_id)
-            ids_to_export.extend(curve_ids)
-            filename = "{}.stp".format(get_specific_part_name(or_part_id))
-            full_path = full_path.format("H" + holoplot_num, filename)
-            export_to_stp(full_path, ids_to_export)
-            rs.DeleteObject(or_part_id)
-            rs.DeleteObject(or_txt_id)
-            rs.DeleteObjects(curve_ids)
+            bb_points = get_min_bb(obj_id)
+            part_name = get_specific_part_name(obj_id)
+            print("Working on: {}".format(part_name))
+            engraving_id = get_engravings(obj_id)
+            thread_ids = get_threads(bb_points)
+            other_text_ids = get_other_texts(bb_points)
+            if engraving_id:
+                lying_xform = get_lying_plane_xform(bb_points)
+                xform_part_id = tranform_objects([obj_id], lying_xform)[0]
+                xform_eng_id = tranform_objects([engraving_id], lying_xform)[0]
+                xform_thread_ids = tranform_objects(thread_ids, lying_xform)
+                xform_oth_txt_ids = tranform_objects(other_text_ids, lying_xform)
+                xform_eng_curve_ids = rs.ExplodeText(xform_eng_id)
+                xform_oth_txt_curve_ids = list()
+                for xform_oth_txt_id in xform_oth_txt_ids:
+                    curve_ids = rs.ExplodeText(xform_oth_txt_id)
+                    xform_oth_txt_curve_ids.extend(curve_ids)
+                ids_to_export = list()
+                ids_to_export.append(xform_part_id)
+                ids_to_export.extend(xform_thread_ids)
+                ids_to_export.extend(xform_eng_curve_ids)
+                ids_to_export.extend(xform_oth_txt_curve_ids)
+                filename = "{}.stp".format(get_specific_part_name(obj_id))
+                full_path = "H:\\Desktop\\projects\\holoplot\\{}\\{}".format("H" + holoplot_num, filename)
+                export_to_stp(full_path, ids_to_export)
+                rs.DeleteObject(xform_part_id)
+                rs.DeleteObject(xform_eng_id)
+                rs.DeleteObjects(xform_thread_ids)
+                rs.DeleteObjects(xform_oth_txt_ids)
+                rs.DeleteObjects(xform_eng_curve_ids)
+                rs.DeleteObjects(xform_oth_txt_curve_ids)
+            else:
+                log_error("Cannot find engraving for {}".format(part_name), holoplot_num)
 
 
-def get_single_part_misc(holoplot_num):
-    ids_to_export = []
-    curve_ids = []
-    part_id = rs.GetObject("Select part to export stp")
-    misc_ids = rs.GetObjects("Select miscellaneous of part")
-    txt_id = search_nearest_text(part_id)
-    misc_ids.append(txt_id)
-    or_part_id, or_misc_ids = orient_to_top(part_id, misc_ids)
-    ids_to_export.append(or_part_id)
-    for or_misc_id in or_misc_ids:
-        if rs.IsText(or_misc_id):
-            curve_ids = rs.ExplodeText(or_misc_id)
-            ids_to_export.extend(curve_ids)
-        else:
-            ids_to_export.append(or_misc_id)
-    filename = "H:\\Desktop\\projects\\holoplot\\{}\\{}.stp".format("H" + holoplot_num,
-                                                                    get_specific_part_name(or_part_id))
-    export_to_stp(filename, ids_to_export)
-    rs.DeleteObject(or_part_id)
-    rs.DeleteObjects(or_misc_ids)
-    rs.DeleteObjects(curve_ids)
+def log_error(message, holo_num):
+    error_file = "parts2stp_err_H{}.txt".format(holo_num)
+    if os.path.exists(error_file):
+        mode = "a"
+    else:
+        mode = "w"
+    with open(error_file, mode=mode) as err_f:
+        err_f.write("[{}]: {}\n".format(datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S"), message))
 
 
 def main():
     holoplot_num = rs.GetString("Holoplot Number")
-    user_choice = rs.GetString("multi or single")
-    if user_choice == "multi":
-        get_multiparts(holoplot_num)
-    elif user_choice == "single":
-        get_single_part_misc(holoplot_num)
-    else:
-        print("Incorrect input")
+    sel_obj_ids = rs.GetObjects("Select parts to export stp")
+    convert_parts_to_stp(holoplot_num, sel_obj_ids)
 
 
 if __name__ == "__main__":
