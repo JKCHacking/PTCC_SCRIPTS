a827db16ea1948c68b49476fe0af0e56dbffde58
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-10-22 16:52:40 +0800

    [RhinoScripts] created a script that will apply holoplot data in json to the spreadsheet file.

---
+++ python_source/RhinoScripts/src/holoplot_partlist_excel.py
@@ -0,0 +1,35 @@
+import os
+import json
+import openpyxl
+from tkinter.filedialog import askopenfilename
+import tkinter
+
+
+def write_json_ws(holo_data, ws):
+    for k, v in holo_data.items():
+        if isinstance(v, dict):
+            if k != "child_parts":
+                data = [v["u_no"], v["s_no"], v["i_no"], v["desc"], v["desc2"], v["quantity"], v["unit"], v["length"],
+                        v["width"],v["height"], v["d_no"]] + v["efal"]
+                ws.append(data)
+            write_json_ws(v, ws)
+
+
+def main():
+    tkinter.Tk().withdraw()
+    filename = askopenfilename(initialdir="H:/Desktop/PTCC_SCRIPTS/python_source/RhinoScripts/src",
+                               filetypes=[("JSON Files", ".json")])
+    if os.path.exists(filename):
+        with open(filename, "r") as jf:
+            holoplot_data = json.load(jf)
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        column_names = ["unique no.", "structure no.", "item no.", "description", "description2", "quantity",
+                        "unit", "length", "width", "height", "drawing no.", "E", "F", "A", "L"]
+        ws.append(column_names)
+        write_json_ws(holoplot_data, ws)
+        wb.save("{} Partlist.xlsx".format(filename.split(".")[0]))
+
+
+if __name__ == "__main__":
+    main()
