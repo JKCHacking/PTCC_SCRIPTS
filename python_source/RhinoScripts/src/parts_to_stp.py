97314ee290c693abd6d95c87154b52c5a33cee74
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2021-11-24 14:12:09 +0800

    [holoplot partslist] REMOVED the slower implementation of getting the minimum bounding box of a holoplot part and added the faster implementation.modified the logic to unmirror the text first before lying it in the plane. modify the logic on how to unmirror by using rotation methods. restored the code to ask for user input if the parts to be converted are mirrored or not.

--- python_source/RhinoScripts/src/parts_to_stp.py
+++ python_source/RhinoScripts/src/parts_to_stp.py
@@ -6,30 +6,6 @@ import Rhino.Geometry as rg
 PRECISION = 3
 
 
-def get_min_bb(obj_id):
-    bb_points = []
-    bb_list = []
-    if rs.IsPolysurface(obj_id):
-        surface_ids = rs.ExplodePolysurfaces(obj_id)
-        # collect all possible bounding box volumes through all surfaces of the model.
-        for surface_id in surface_ids:
-            if rs.IsSurfaceTrimmed(surface_id):
-                rs.Command("CPlane _O _SelID {}".format(surface_id), echo=False)
-                current_cplane = rs.ViewCPlane()
-                bb = rs.BoundingBox(obj_id, view_or_plane=current_cplane)
-                box_id = rs.AddBox(bb)
-                volume = rs.SurfaceVolume(box_id)
-                rs.DeleteObject(box_id)
-                bb_list.append((volume, bb))
-        rs.DeleteObjects(surface_ids)
-        # determine the best bounding box for the profile by determining least volume.
-        best_bb = min(bb_list, key=lambda x: (x[0][0], x[0][1]))
-        bb_points = best_bb[1]
-    elif rs.IsBlockInstance(obj_id):
-        pass
-    return bb_points
-
-
 def export_to_stp(stp_filename, obj_ids):
     rs.SelectObjects(obj_ids)
     rs.Command("_-Export {} _Enter".format(stp_filename), False)
@@ -78,7 +54,7 @@ def get_threads(sel_thread_ids, bb_points):
     return thread_ids
 
 
-def convert_parts_to_stp(holoplot_num, sel_obj_ids, sel_thread_ids, sel_other_text_ids):
+def convert_parts_to_stp(holoplot_num, sel_obj_ids, sel_thread_ids, sel_other_text_ids, mirror_text):
     already_done = []
     for obj_id in sel_obj_ids:
         part_name = get_specific_part_name(obj_id)
@@ -86,91 +62,70 @@ def convert_parts_to_stp(holoplot_num, sel_obj_ids, sel_thread_ids, sel_other_te
         if rs.IsPolysurface(obj_id) and\
                 "threaded" not in part_name and\
                 part_name not in already_done:
-            bb_points = get_min_bb(obj_id)
             engraving_id = get_engravings(obj_id)
+            bb_points = rs.BoundingBox(obj_id, rs.TextObjectPlane(engraving_id))
             thread_ids = get_threads(sel_thread_ids, bb_points)
             other_text_ids = get_other_texts(sel_other_text_ids, bb_points)
 
             if engraving_id:
+                # unmirror all text objects if they are mirrored.
+                copy_obj_id = rs.CopyObject(obj_id)
+                copy_thread_ids = rs.CopyObjects(thread_ids)
+                copy_engraving_id = rs.CopyObject(engraving_id)
+                copy_oth_txt_ids = rs.CopyObjects(other_text_ids)
+                if mirror_text:
+                    unmirror(copy_engraving_id)
+                    for other_text_id in copy_oth_txt_ids:
+                        unmirror(other_text_id)
                 # transform plane
-                xform_plane = rg.Transform.PlaneToPlane(rs.TextObjectPlane(engraving_id), rs.WorldXYPlane())
-                xform_part_id = rs.TransformObject(obj_id, xform_plane, True)
-                xform_eng_id = rs.TransformObject(engraving_id, xform_plane, True)
-                xform_thread_ids = rs.TransformObjects(thread_ids, xform_plane, True)
-                xform_oth_txt_ids = rs.TransformObjects(other_text_ids, xform_plane, True)
-                # rotate 180 degrees
-                if is_mirrored(xform_eng_id):
-                    xform_rotate = rs.XformRotation2(180, rs.WorldXYPlane().XAxis,
-                                                     rs.WorldXYPlane().Origin)
-                    rs.TransformObject(xform_part_id, xform_rotate)
-                    rs.TransformObject(xform_eng_id, xform_rotate)
-                    rs.TransformObjects(xform_thread_ids, xform_rotate)
-                    rs.TransformObjects(xform_oth_txt_ids, xform_rotate)
+                xform_plane = rg.Transform.PlaneToPlane(rs.TextObjectPlane(copy_engraving_id),
+                                                        rs.WorldXYPlane())
+                rs.TransformObject(copy_obj_id, xform_plane)
+                rs.TransformObject(copy_engraving_id, xform_plane)
+                rs.TransformObjects(copy_oth_txt_ids, xform_plane)
+                rs.TransformObjects(copy_thread_ids, xform_plane)
 
-                # make it horizontal or make the X axis point to the right
-                while round(rs.TextObjectPlane(xform_eng_id).XAxis[0], PRECISION) <= 0:
-                    rs.RotateObject(xform_part_id, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
-                    rs.RotateObject(xform_eng_id, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
-                    rs.RotateObjects(xform_thread_ids, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
-                    rs.RotateObjects(xform_oth_txt_ids, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
-
-                # unmirror all text objects if they are in mirror form.
-                if is_mirrored(xform_eng_id):
-                    unmirror(xform_eng_id)
-                    for xform_oth_txt_id in xform_oth_txt_ids:
-                        unmirror(xform_oth_txt_id)
                 # make the XAxis point to the right and make the YAxis point up
-                while round(rs.TextObjectPlane(xform_eng_id).XAxis[0], PRECISION) <= 0 and \
-                        round(rs.TextObjectPlane(xform_eng_id).YAxis[1], PRECISION) <= 0:
-                    rs.RotateObject(xform_part_id, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
-                    rs.RotateObject(xform_eng_id, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
-                    rs.RotateObjects(xform_thread_ids, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
-                    rs.RotateObjects(xform_oth_txt_ids, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
+                while round(rs.TextObjectPlane(copy_engraving_id).XAxis[0], PRECISION) <= 0 and \
+                        round(rs.TextObjectPlane(copy_engraving_id).YAxis[1], PRECISION) <= 0:
+                    rs.RotateObject(copy_obj_id, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
+                    rs.RotateObject(copy_engraving_id, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
+                    rs.RotateObjects(copy_thread_ids, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
+                    rs.RotateObjects(copy_oth_txt_ids, rs.WorldXYPlane().Origin, 90, rs.WorldXYPlane().Normal)
 
-                xform_eng_curve_ids = rs.ExplodeText(xform_eng_id)
-                xform_oth_txt_curve_ids = list()
-                for xform_oth_txt_id in xform_oth_txt_ids:
-                    curve_ids = rs.ExplodeText(xform_oth_txt_id)
-                    xform_oth_txt_curve_ids.extend(curve_ids)
+                eng_curve_ids = rs.ExplodeText(copy_engraving_id)
+                oth_txt_curve_ids = list()
+                for oth_txt_id in copy_oth_txt_ids:
+                    curve_ids = rs.ExplodeText(oth_txt_id)
+                    oth_txt_curve_ids.extend(curve_ids)
                 ids_to_export = list()
-                ids_to_export.append(xform_part_id)
-                ids_to_export.extend(xform_thread_ids)
-                ids_to_export.extend(xform_eng_curve_ids)
-                ids_to_export.extend(xform_oth_txt_curve_ids)
+                ids_to_export.append(copy_obj_id)
+                ids_to_export.extend(copy_thread_ids)
+                ids_to_export.extend(eng_curve_ids)
+                ids_to_export.extend(oth_txt_curve_ids)
                 filename = "{}_00.stp".format(get_specific_part_name(obj_id))
                 full_path = "H:\\Desktop\\projects\\holoplot\\{}\\{}".format("H" + holoplot_num, filename)
                 export_to_stp(full_path, ids_to_export)
-                rs.DeleteObject(xform_part_id)
-                rs.DeleteObject(xform_eng_id)
-                rs.DeleteObjects(xform_thread_ids)
-                rs.DeleteObjects(xform_oth_txt_ids)
-                rs.DeleteObjects(xform_eng_curve_ids)
-                rs.DeleteObjects(xform_oth_txt_curve_ids)
+                rs.DeleteObject(copy_obj_id)
+                rs.DeleteObject(copy_engraving_id)
+                rs.DeleteObjects(copy_thread_ids)
+                rs.DeleteObjects(copy_oth_txt_ids)
+                rs.DeleteObjects(eng_curve_ids)
+                rs.DeleteObjects(oth_txt_curve_ids)
             else:
                 log_error("Cannot find engraving for {}".format(part_name), holoplot_num)
             already_done.append(part_name)
 
 
 def unmirror(text_id):
-    rs.Command("Top")
     # mirrored
     start = rs.TextObjectPoint(text_id)
-    end = start + (rs.TextObjectPlane(text_id).YAxis * rs.TextObjectHeight(text_id) * -1)
     mid = start + (rs.TextObjectPlane(text_id).YAxis * (rs.TextObjectHeight(text_id) / 2) * -1)
-    rs.MirrorObject(text_id, start, end)
+    rs.RotateObject(text_id, mid, 180, rs.TextObjectPlane(text_id).YAxis)
     rs.RotateObject(text_id,
                     mid,
                     180,
-                    rs.WorldXYPlane().Normal)
-    rs.Command("Perspective")
-
-
-def is_mirrored(text_id):
-    mirrored = False
-    if (rs.TextObjectPlane(text_id).XAxis[0] <= 0 < rs.TextObjectPlane(text_id).YAxis[1]) or \
-            (rs.TextObjectPlane(text_id).XAxis[0] > 0 >= rs.TextObjectPlane(text_id).YAxis[1]):
-        mirrored = True
-    return mirrored
+                    rs.TextObjectPlane(text_id).Normal)
 
 
 def log_error(message, holo_num):
@@ -193,8 +148,8 @@ def reset_rhino_view():
 def main():
     reset_rhino_view()
     holoplot_num = rs.GetString("Holoplot Number")
-    items = ("Threads", "No", "Yes"), ("OtherTexts", "No", "Yes")
-    result = rs.GetBoolean("Options", items, (False, False))
+    items = ("Threads", "No", "Yes"), ("OtherTexts", "No", "Yes"), ("MirrorTexts", "No", "Yes")
+    result = rs.GetBoolean("Options", items, (False, False, False))
     sel_obj_ids = rs.GetObjects("Select parts to export stp")
 
     thread_ids = []
@@ -203,7 +158,7 @@ def main():
         thread_ids = rs.GetObjects("Select threads")
     if result[1]:
         other_text_ids = rs.GetObjects("Select other texts")
-    convert_parts_to_stp(holoplot_num, sel_obj_ids, thread_ids, other_text_ids)
+    convert_parts_to_stp(holoplot_num, sel_obj_ids, thread_ids, other_text_ids, result[2])
 
 
 if __name__ == "__main__":
