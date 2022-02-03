874bbc986f047d83cfdfce083512daa9f16a0212
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2022-02-03 13:02:38 +0800

    [accounting macro] * added atc_code and categories in liquidation file.

--- python_source/LibreOfficeMacro/src/accounting_macro.py
+++ python_source/LibreOfficeMacro/src/accounting_macro.py
@@ -37,12 +37,14 @@ def get_transactions():
             supplier = input_sheet.getCellByPosition(4, curr_row).getString().strip()
             particulars = input_sheet.getCellByPosition(5, curr_row).getString().strip()
             is_qualified = input_sheet.getCellByPosition(6, curr_row).getString().strip()
-            cash_in_bank = input_sheet.getCellByPosition(7, curr_row).getString().strip()
-            ewt = input_sheet.getCellByPosition(8, curr_row).getString().strip()
-            in_tax = input_sheet.getCellByPosition(9, curr_row).getString().strip()
+            atc_code = input_sheet.getCellByPosition(7, curr_row).getString().strip()
+            categories = input_sheet.getCellByPosition(8, curr_row).getString().strip()
+            cash_in_bank = input_sheet.getCellByPosition(9, curr_row).getString().strip()
+            ewt = input_sheet.getCellByPosition(10, curr_row).getString().strip()
+            in_tax = input_sheet.getCellByPosition(11, curr_row).getString().strip()
 
             # get the data from the account titles
-            acc_col = 10
+            acc_col = 12
             acc_dict = {}
             while input_sheet.getCellByPosition(acc_col, 4).getString():
                 acc_name = input_sheet.getCellByPosition(acc_col, 4).getString().strip()
@@ -58,6 +60,8 @@ def get_transactions():
                 supplier,
                 particulars,
                 is_qualified,
+                atc_code,
+                categories,
                 cash_in_bank,
                 ewt,
                 in_tax,
@@ -79,6 +83,8 @@ def load_transactions(*args):
             input_sheet.getCellByPosition(0, row).setString(trans.date.strftime("%Y-%b-%d"))
             input_sheet.getCellByPosition(1, row).setString(trans.cv_no)
             input_sheet.getCellByPosition(3, row).setString(trans.supplier)
+            input_sheet.getCellByPosition(7, row).setString(trans.atc_code)
+            input_sheet.getCellByPosition(8, row).setString(trans.categories)
             input_sheet.getCellByPosition(13, row).setString(trans.si_no)
             net_purchases = 0
             account_titles = []
@@ -97,6 +103,8 @@ def load_transactions(*args):
                     input_sheet.getCellByPosition(0, row + 1).setString(trans.date.strftime("%Y-%b-%d"))
                     input_sheet.getCellByPosition(1, row + 1).setString(trans.cv_no)
                     input_sheet.getCellByPosition(3, row + 1).setString(trans.supplier)
+                    input_sheet.getCellByPosition(7, row + 1).setString(trans.atc_code)
+                    input_sheet.getCellByPosition(8, row + 1).setString(trans.categories)
                     input_sheet.getCellByPosition(13, row + 1).setString(trans.si_no)
                     input_sheet.getCellByPosition(11, row + 1).setValue(trans.ewt)
                     row += 1
@@ -106,11 +114,11 @@ def load_transactions(*args):
 
 class Transaction:
     __slots__ = ("date", "cv_no", "si_date", "si_no", "supplier",
-                 "particulars", "is_qualified", "cash_in_bank",
-                 "ewt", "in_tax", "accounts")
+                 "particulars", "is_qualified", "atc_code", "categories",
+                 "cash_in_bank", "ewt", "in_tax", "accounts")
 
     def __init__(self, date, cv_no, si_date, si_no, supplier,
-                 particulars, is_qualified, cash_in_bank,
+                 particulars, is_qualified, atc_code, categories, cash_in_bank,
                  ewt, in_tax, accounts):
         self.date = datetime.datetime.strptime(date, "%Y-%b-%d").date()
         self.cv_no = cv_no
@@ -119,6 +127,8 @@ class Transaction:
         self.supplier = supplier
         self.particulars = particulars
         self.is_qualified = True if is_qualified == "Yes" else False
+        self.atc_code = atc_code
+        self.categories = categories
         self.cash_in_bank = atof(cash_in_bank) if cash_in_bank else 0
         self.ewt = atof(ewt) if ewt else 0
         self.in_tax = atof(in_tax) if in_tax else 0
