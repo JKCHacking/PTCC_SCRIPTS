6d6d097c681b65ac3b4d497c2332380c5d3fe085
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2022-02-17 10:07:37 +0800

    [BRICSCAD SCRIPTS][CAD OBJECT COUNTER] * added functions for getting the solids from modelspace * added function for removing duplicate objects that exists above an object. * added function for getting the length of the object using UCS aligned bounding box.

--- python_source/cad_object_analyzer.py
+++ python_source/cad_object_analyzer.py
@@ -6,6 +6,10 @@ import csv
 import os
 
 
+SS_NAME = "SSOBJECTS"
+CROSSWINDOW_SEL_MODE = 1
+
+
 def get_cad_application():
     b_cad_guid = "BricscadApp.AcadApplication"
     try:
@@ -23,25 +27,62 @@ def get_bounding_box(obj):
     return min_pt, max_pt
 
 
+def get_length(obj):
+    min_pt, max_pt = get_bounding_box(obj)
+    x_dist = max_pt[0][0] - min_pt[0][0]
+    y_dist = max_pt[0][1] - min_pt[0][1]
+    z_dist = max_pt[0][2] - min_pt[0][2]
+    length = round(max([x_dist, y_dist, z_dist]), 1)
+    return length
+
+
+def get_solids(doc):
+    solids = []
+    model_space = doc.ModelSpace
+    for obj in model_space:
+        if obj.ObjectName == "AcDb3dSolid":
+            solids.append(obj)
+    return solids
+
+
+def remove_duplicate_objects(doc):
+    solids = get_solids(doc)
+    to_delete = []
+    for solid in solids:
+        if solid not in to_delete:
+            min_pt, max_pt = get_bounding_box(solid)
+            selection_set = doc.SelectionSets.Add(SS_NAME)
+            selection_set.Select(CROSSWINDOW_SEL_MODE, min_pt, max_pt)
+            if selection_set.Count > 1:
+                print("{} duplicates found for {}".format(selection_set.Count, solid.Handle))
+                for i in range(1, selection_set.Count):
+                    sel_obj = selection_set.Item(i)
+                    to_delete.append(sel_obj)
+            # so you can use the same SS_NAME
+            selection_set.Delete()
+    for solid in to_delete:
+        try:
+            solid.Delete()
+        except COMError:
+            pass
+
+
 def main():
     b_cad = get_cad_application()
     doc = b_cad.ActiveDocument
-    modelSpace = doc.ModelSpace
     groups = {}
-    for obj in modelSpace:
-        if obj.ObjectName == "AcDb3dSolid":
-            min_pt, max_pt = get_bounding_box(obj)
-            x_dist = max_pt[0][0] - min_pt[0][0]
-            y_dist = max_pt[0][1] - min_pt[0][1]
-            z_dist = max_pt[0][2] - min_pt[0][2]
-            length = round(max([x_dist, y_dist, z_dist]), 1)
-            if length != 0:
-                if length in groups:
-                    groups.update({length: groups[length] + 1})
-                else:
-                    groups.update({length: 1})
+    # removing duplicate objects
+    remove_duplicate_objects(doc)
+    solids = get_solids(doc)
+    for solid in solids:
+        length = get_length(solid)
+        if length != 0:
+            if length in groups:
+                groups.update({length: groups[length] + 1})
             else:
-                print("solid with 0 length: {}".format(obj.Handle))
+                groups.update({length: 1})
+        else:
+            print("solid with 0 length: {}".format(solid.Handle))
 
     total = 0
     output = "H://Desktop//projects//ObjectCounter"
