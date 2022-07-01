78b7ab11e2f5171f01bec03c72a9e21550c54dc5
joshnee.cunanan <joshnee.kim.cunanan@gmail.com>
2022-07-01 14:30:47 +0800

    [Inventor to DWG Converter] * modified input implementation to ask input and create an output folder to add the generated DWG. removed the Constants file since it is not anymore needed.

--- python/Architecture/CAD/ViewsExtractor/ViewsExtractor/src/constants.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Constants:
-    SRC_DIR = os.path.dirname(os.path.realpath(__file__))
-    APP_DIR = os.path.dirname(SRC_DIR)
-    PROJ_DIR = os.path.dirname(APP_DIR)
-    INPUT_DIR = os.path.join(PROJ_DIR, "input")
-    OUTPUT_DIR = os.path.join(PROJ_DIR, "output")
